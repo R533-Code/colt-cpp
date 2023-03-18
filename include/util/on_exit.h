0e71699777a077033cf145eafcea3c05421adfcf
R533-Code <raphaeldibnehmepc@gmail.com>
2023-03-18 10:37:33 +0200

    Added ON_EXIT for action required after scope exit

---
+++ include/util/on_exit.h
@@ -0,0 +1,38 @@
+#ifndef HG_COLT_ON_EXIT
+#define HG_COLT_ON_EXIT
+
+namespace clt::details
+{
+  template<typename Fun>
+  struct ScopeGuard
+  {
+    Fun fn;
+
+    ScopeGuard(Fun&& fn) noexcept
+      : fn(std::forward<Fun>(fn)) {}
+
+    ~ScopeGuard() noexcept { fn(); }
+  };
+
+  enum class ScopeGuardOnExit {};
+  
+  template<typename Fun>
+  ScopeGuard<Fun> operator+(ScopeGuardOnExit, Fun&& fn) noexcept
+  {
+    return ScopeGuard<Fun>(std::forward<Fun>(fn));
+  }
+}
+
+/// @brief Register an action to be called at the end of the scope.
+/// Example:
+/// ```c++
+/// {
+///		auto hello = 10;
+///		ON_EXIT {
+///			std::cout << hello;
+///		}; // <- do not forget the semicolon
+/// }
+/// ```
+#define ON_EXIT auto COLT_CONCAT(SCOPE_EXIT_HELPER, __LINE__) = clt::details::ScopeGuardOnExit() + [&]() 
+
+#endif //!HG_COLT_ON_EXIT
