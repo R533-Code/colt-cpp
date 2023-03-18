bd08b2f0b502b6497784a0d6f21b7caff30ca025
R533-Code <raphaeldibnehmepc@gmail.com>
2023-03-18 10:51:05 +0200

    Added comments

--- include/util/benchmark.h
+++ include/util/benchmark.h
@@ -16,16 +16,15 @@
 #include "./macro.h"
 #include "./on_exit.h"
 
-// Generate unique ID using filename and line number,
-// registers begin and end of scope using ID.
-// We can then reconstruct time-line.
 namespace clt::bench
 {
+  /// @brief Time point type from steady_clock
   using time_point = std::chrono::time_point<std::chrono::steady_clock>;
+  /// @brief Duration type from steady_clock
   using duration = std::chrono::steady_clock::duration;
   namespace tm = std::chrono;
 
-  /// @brief Timer class, whic
+  /// @brief Timer class, records elapsed time since construction
   class Timer
   {
     /// @brief The start time
@@ -74,12 +73,21 @@ namespace clt::bench
     u64 thread_id;
   };
 
-  class GlobalInstrumentator
+  /// @brief Class responsible of storing profiles results
+  class Instrumentator
   {
+    /// @brief Vector to store the ProfileResult
     std::vector<ProfileResult> profiles {};
+    /// @brief Mutex to avoid data-races
     mutable std::mutex mtx {};
 
     template<meta::StringLiteral lit>
+    /// @brief Writes a profile to a file in the "chrome://tracing" format
+    /// @tparam lit Literal string to append before format string
+    /// @param file The file to which to write
+    /// @param start Buffer where to write the start time as a string
+    /// @param durat Buffer where to write the duration as a string
+    /// @param profile The profile to write
     void write_profile(FILE* file, char (*start)[24], char (*durat)[24], const ProfileResult& profile) const noexcept
     {
       *std::to_chars(*start, *start + 24,
@@ -102,14 +110,25 @@ namespace clt::bench
     }
 
   public:
-    GlobalInstrumentator() noexcept = default;
+    // No copy constructor
+    Instrumentator(const Instrumentator&) = delete;
+    //No move constructor
+    Instrumentator(Instrumentator&&) = delete;
 
+    /// @brief Saves a profile result
+    /// @param src The source of the profile
+    /// @param tm The start time
+    /// @param dur The duration
+    /// @param thread_id The thread ID
     void save_profile(const ProfileSource& src, time_point tm, duration dur, std::thread::id thread_id = std::this_thread::get_id()) noexcept
     {
       auto lck = std::scoped_lock(mtx);
       profiles.emplace_back(src, tm, dur, std::hash<std::thread::id>{}(thread_id));
     }
 
+    /// @brief Writes saved profile in the "chrome://tracing" format to file at 'path'
+    /// @param path The path name (preferably ending with .json)
+    /// @return True if successful
     bool write_tracing(lstring path) const noexcept
     {
       auto lck = std::scoped_lock(mtx);
@@ -133,20 +152,28 @@ namespace clt::bench
 
   namespace details
   {
-    inline GlobalInstrumentator GlobalInstrument = {};
+    /// @brief Responsible of holding all profile results
+    inline Instrumentator GlobalInstrument;
   }
 
-  inline void save_tracing_to(const char* path) noexcept
+  /// @brief Saves the profiles stored in the global Instrumentator to file at 'path' in "chrome://tracing" format
+  /// @param path The path to which to write the profiles
+  /// @return True if successful
+  inline bool save_tracing_to(const char* path) noexcept
   {
-    details::GlobalInstrument.write_tracing(path);
+    return details::GlobalInstrument.write_tracing(path);
   }
 }
 
 #ifndef COLT_NO_PROFILE
+  /// @brief Profiles a scope, with name 'name'. NOT AN INLINE MACRO.
   #define COLT_PROFILE_SCOPE(name) clt::bench::Timer COLT_CONCAT(__TIMER_, __LINE__); static constexpr clt::bench::ProfileSource COLT_CONCAT(__TIMER_SRC_, __LINE__) = { name, COLT_FILENAME, COLT_LINE_NUM }; ON_EXIT { clt::bench::details::GlobalInstrument.save_profile(COLT_CONCAT(__TIMER_SRC_, __LINE__), COLT_CONCAT(__TIMER_, __LINE__).get_start_time(), COLT_CONCAT(__TIMER_, __LINE__).get_elapsed_time()); }
+  /// @brief Profiles a function. NOT AN INLINE MACRO.
   #define COLT_PROFILE_FN() COLT_PROFILE_SCOPE(COLT_FUNCTION_NAME)
 #else
+  /// @brief Does nothing.
   #define COLT_PROFILE_SCOPE(name) do {} while (0)
+  /// @brief Does nothing.
   #define COLT_PROFILE_FN() do {} while (0)
 #endif
 
