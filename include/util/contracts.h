331e171b96cb4e868657ae7a4a02b64fe133e8f0
R533-Code <raphaeldibnehmepc@gmail.com>
2023-03-19 12:59:57 +0200

    Added ';'

--- include/util/contracts.h
+++ include/util/contracts.h
@@ -6,10 +6,10 @@
 /// @brief Creates a precondition for a function body
 #define COLT_PRE(condition) { assert(condition);
 /// @brief Creates a postcondition for a function body
-#define COLT_POST(condition) assert(condition); }
+#define COLT_POST(condition) assert(condition); };
 /// @brief Begins a postcondition if a function does not have a precondition
 #define COLT_BEGIN() {
 /// @brief Ends a precondition if a function does not have a postcondition
-#define COLT_END() }
+#define COLT_END() };
 
 #endif //!HG_COLT_CONTRACTS
