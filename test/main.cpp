27cd76aea6fd93474c7a7a1f94b2740b1dc84600
R533-Code <raphaeldibnehmepc@gmail.com>
2023-03-20 12:28:45 +0200

    Tested contracts

--- test/main.cpp
+++ test/main.cpp
@@ -2,16 +2,12 @@
 #include "util/benchmark.h"
 
 using namespace std::chrono_literals;
+using namespace clt;
 
 int main(int argc, int argv)
 {
   std::atexit([]() { clt::bench::save_tracing_to("Test.json"); });
 
-  COLT_PROFILE_FN();
-  std::this_thread::sleep_for(10ms);
-  {
-    COLT_PROFILE_SCOPE("main scope");
-    std::this_thread::sleep_for(15ms);
-  }
-  std::this_thread::sleep_for(10ms);
+  BYTE a = { 10 };
+  a.chg_n(8, true);
 }
