8c5e5cde069aed179d4c5e5d7d312d078f2d1242
R533-Code <raphaeldibnehmepc@gmail.com>
2023-03-20 18:28:00 +0200

    Tested io/print

--- test/main.cpp
+++ test/main.cpp
@@ -1,13 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "util/benchmark.h"
+#include "io/print.h"
 
 using namespace std::chrono_literals;
 using namespace clt;
+using namespace clt::io;
 
 int main(int argc, int argv)
 {
   std::atexit([]() { clt::bench::save_tracing_to("Test.json"); });
 
-  BYTE a = { 10 };
-  a.chg_n(8, true);
+  {
+    COLT_PROFILE_SCOPE("fmt::print");
+    print_message("Hello {}", "World!");
+  }
 }
