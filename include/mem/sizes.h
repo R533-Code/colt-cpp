832cf256cc64bb02e24bccfe9dea894bd86cda38
R533-Code <raphaeldibnehmepc@gmail.com>
2023-03-24 08:51:34 +0200

    Added size helpers

---
+++ include/mem/sizes.h
@@ -0,0 +1,93 @@
+#ifndef HG_COLT_SIZES
+#define HG_COLT_SIZES
+
+#include <ratio>
+#include "../meta/traits.h"
+#include "../util/typedefs.h"
+
+namespace clt
+{
+  /// @brief Byte ratio
+  using B         = std::ratio<1, 1>;
+  /// @brief Byte ratio
+  using Byte      = std::ratio<1, 1>;
+  /// @brief Kibibyte ratio
+  using KiB       = std::ratio<1024, 1>;
+  /// @brief Kibibyte ratio
+  using KibiByte  = std::ratio<1024, 1>;
+  /// @brief Mebibyte ratio
+  using MiB       = std::ratio<1024 * 1024, 1>;
+  /// @brief Mebibyte ratio
+  using MebiByte  = std::ratio<1024 * 1024, 1>;
+  /// @brief Gibibyte ratio
+  using GiB       = std::ratio<1024 * 1024 * 1024, 1>;
+  /// @brief Gibibyte ratio
+  using GibiByte  = std::ratio<1024 * 1024 * 1024, 1>;  
+
+  template<typename RatioT> requires meta::StdRatio<RatioT>
+  /// @brief Class responsible of holding byte sizes
+  /// @tparam RatioT The ratio to bytes
+  class size
+  {
+    /// @brief The count of bytes / RatioT::num
+    u64 sz;
+
+  public:
+    /// @brief The ratio to byte of the size
+    using ratio = RatioT;
+
+    /// @brief Constructs a size from a count of RatioT
+    /// @param sz The size
+    constexpr size(u64 sz) noexcept
+      : sz(sz) {}
+
+    template<typename rt> requires (RatioT::num < rt::num)
+    /// @brief Constructs a size from another size only when the conversion is not lossy.
+    /// Use size_cast for lossy conversions.
+    /// @tparam rt The ratio of the other size
+    /// @param sz The other size
+    constexpr size(size<rt> sz) noexcept
+      : sz(sz.sz * rt::num) {}
+
+    /// @brief Converts the size to a byte count
+    /// @return Byte count
+    constexpr u64 to_bytes() const noexcept { return sz * RatioT::num; }
+
+    /// @brief The value of the size
+    /// @return The value of the size
+    constexpr u64 value() const noexcept { return sz; }
+
+    template<typename U> requires meta::StdRatio<U>
+    friend class size;
+  };
+
+  template<typename Ratio, typename From> requires meta::StdRatio<Ratio> && meta::StdRatio<From>
+  /// @brief Converts from a size type to another one (performing lossy conversions)
+  /// @tparam Ratio The ratio to convert to
+  /// @tparam From The ratio to convert from
+  /// @param value The value to convert
+  /// @return The converted value
+  constexpr size<Ratio> size_cast(size<From> value) noexcept
+  {
+    return (value.value() * From::num) / Ratio::num;
+  }
+  
+  /// @brief Creates a Byte size
+  /// @param i The size count
+  /// @return Byte size
+  constexpr size<B>   operator""_B(u64 i)   noexcept { return size<B>(i); }
+  /// @brief Creates a Kibibyte size
+  /// @param i The size count
+  /// @return Kibibyte size
+  constexpr size<KiB> operator""_KiB(u64 i) noexcept { return size<KiB>(i); }
+  /// @brief Creates a Mebibyte size
+  /// @param i The size count
+  /// @return Mebibyte size
+  constexpr size<MiB> operator""_MiB(u64 i) noexcept { return size<MiB>(i); }
+  /// @brief Creates a Gibibyte size
+  /// @param i The size count
+  /// @return Gibibyte size
+  constexpr size<GiB> operator""_GiB(u64 i) noexcept { return size<GiB>(i); }
+}
+
+#endif //!HG_COLT_SIZES
