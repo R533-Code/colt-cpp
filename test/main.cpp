38f34746b7b6b38adc583241a2921dc8e7702217
R533-Code <raphaeldibnehmepc@gmail.com>
2023-03-25 15:35:06 +0200

    Added new include

--- test/main.cpp
+++ test/main.cpp
@@ -1,15 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "util/benchmark.h"
 #include "io/print.h"
+#include "io/input.h"
+#include "mem/simple_alloc.h"
 
 using namespace std::chrono_literals;
 using namespace clt;
 using namespace clt::io;
+using namespace clt::mem;
 
 int main(int argc, int argv)
 {
   std::atexit([]() { clt::bench::save_tracing_to("Test.json"); });
-
+  
   {
     COLT_PROFILE_SCOPE("fmt::print");
     print_message("Hello {}", "World!");
