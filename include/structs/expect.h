51bfb74287dae9ca4db9aff80d7962535f0a969a
R533-Code <raphaeldibnehmepc@gmail.com>
2023-04-01 18:55:04 +0300

    Renamed to simplify use

--- include/structs/expect.h
+++ include/structs/expect.h
@@ -7,16 +7,16 @@ namespace clt
 {
   template<typename ExpectedTy, typename ErrorTy>
   /// @brief A helper class that can hold either a valid value or an error.
-  /// This class can be seen as an Optional, that carries error informations.
-  /// To document source code more, to constructs an Expected containing
+  /// This class can be seen as an Option, that carries error informations.
+  /// To document source code more, to constructs an Expect containing
   /// an error, clt::Error is passed to the constructor.
-  /// The default constructor of an Expected default constructs an expected value:
-  /// While this might seen counterintuitive, this class is named Expected, as it
+  /// The default constructor of an Expect default constructs an expected value:
+  /// While this might seen counterintuitive, this class is named Expect, as it
   /// will most likely contain the expected value, not an error.
   ///
   /// Example Usage:
   /// ```C++
-  /// Expected<int, const char*> div(int a, int b)
+  /// Expect<int, const char*> div(int a, int b)
   /// {
   ///   if (b != 0)
   ///     return a / b;
@@ -25,7 +25,7 @@ namespace clt
   /// ```
   /// @tparam ExpectedTy The expected type
   /// @tparam ErrorTy The error type
-  class Expected
+  class Expect
   {
     /// @brief Buffer for both error type and expected value
     union
@@ -36,23 +36,23 @@ namespace clt
       ErrorTy error_v;
     };
 
-    /// @brief True if an error is stored in the Expected
+    /// @brief True if an error is stored in the Expect
     bool is_error_v;
 
   public:
-    /// @brief Default constructs an error in the Expected
+    /// @brief Default constructs an error in the Expect
     /// @param  ErrorT tag
-    constexpr Expected(meta::ErrorT)
+    constexpr Expect(meta::ErrorT)
       noexcept(std::is_nothrow_constructible_v<ErrorTy>)
       : is_error_v(true)
     {
       new(&error_v) ErrorTy();
     }
 
-    /// @brief Copy constructs an error in the Expected
+    /// @brief Copy constructs an error in the Expect
     /// @param  ErrorT tag
     /// @param value The value to copy
-    constexpr Expected(meta::ErrorT, meta::copy_trivial_t<const ErrorTy&> value)
+    constexpr Expect(meta::ErrorT, meta::copy_trivial_t<const ErrorTy&> value)
       noexcept(std::is_nothrow_copy_constructible_v<ErrorTy>)
       : is_error_v(true)
     {
@@ -60,10 +60,10 @@ namespace clt
     }
 
 
-    /// @brief Move constructs an error in the Expected
+    /// @brief Move constructs an error in the Expect
     /// @param  ErrorT tag
     /// @param to_move The value to move
-    constexpr Expected(meta::ErrorT, ErrorTy&& to_move)
+    constexpr Expect(meta::ErrorT, ErrorTy&& to_move)
       noexcept(std::is_nothrow_move_constructible_v<ErrorTy>)
       requires (!std::is_trivial_v<ErrorTy>)
       : is_error_v(true)
@@ -72,38 +72,38 @@ namespace clt
     }
 
     template<typename... Args>
-    /// @brief Constructs an error in place in the Expected
+    /// @brief Constructs an error in place in the Expect
     /// @tparam ...Args Parameter pack
     /// @param  InPlaceT tag
     /// @param  ErrorT tag
     /// @param ...args Argument pack forwarded to the constructor
-    constexpr Expected(meta::InPlaceT, meta::ErrorT, Args&&... args)
+    constexpr Expect(meta::InPlaceT, meta::ErrorT, Args&&... args)
       noexcept(std::is_nothrow_constructible_v<ErrorTy, Args...>)
       : is_error_v(true)
     {
       new(&error_v) ErrorTy(std::forward<Args>(args)...);
     }
 
-    /// @brief Default constructs an expected value in the Expected
-    constexpr Expected()
+    /// @brief Default constructs an expected value in the Expect
+    constexpr Expect()
       noexcept(std::is_default_constructible_v<ExpectedTy>)
       : is_error_v(false)
     {
       new(&expected) ExpectedTy();
     }
 
-    /// @brief Copy constructs an expected value in the Expected
+    /// @brief Copy constructs an expected value in the Expect
     /// @param value The value to copy
-    constexpr Expected(meta::copy_trivial_t<const ExpectedTy&> value)
+    constexpr Expect(meta::copy_trivial_t<const ExpectedTy&> value)
       noexcept(std::is_nothrow_copy_constructible_v<ExpectedTy>)
       : is_error_v(false)
     {
       new(&expected) ExpectedTy(value);
     }
 
-    /// @brief Move constructs an expected value in the Expected
+    /// @brief Move constructs an expected value in the Expect
     /// @param to_move The value to move
-    constexpr Expected(ExpectedTy&& to_move)
+    constexpr Expect(ExpectedTy&& to_move)
       noexcept(std::is_nothrow_move_constructible_v<ExpectedTy>)
       requires (!std::is_trivial_v<ExpectedTy>)
     : is_error_v(false)
@@ -112,12 +112,12 @@ namespace clt
     }
 
     template<typename... Args, typename Ty>
-    /// @brief Constructs an expected value in place in the Expected
+    /// @brief Constructs an expected value in place in the Expect
     /// @tparam ...Args Parameter pack
     /// @param  InPlaceT tag
     /// @param  ErrorT tag
     /// @param ...args Argument pack forwarded to the constructor
-    constexpr Expected(meta::InPlaceT, Ty&& arg, Args&&... args)
+    constexpr Expect(meta::InPlaceT, Ty&& arg, Args&&... args)
       noexcept(std::is_nothrow_constructible_v<ExpectedTy, Ty, Args...>)
       requires (!std::is_same_v<Ty, meta::ErrorT>)
       : is_error_v(false)
@@ -125,9 +125,9 @@ namespace clt
       new(&expected) ExpectedTy(std::forward<Ty>(arg), std::forward<Args>(args)...);
     }
 
-    /// @brief Copy constructs an Expected
-    /// @param copy The Expected to copy
-    constexpr Expected(const Expected& copy)
+    /// @brief Copy constructs an Expect
+    /// @param copy The Expect to copy
+    constexpr Expect(const Expect& copy)
       noexcept(std::is_nothrow_copy_constructible_v<ExpectedTy>
         && std::is_nothrow_copy_constructible_v<ErrorTy>)
       : is_error_v(copy.is_error_v)
@@ -139,9 +139,9 @@ namespace clt
     }
 
     /// @brief Copy assignment operator
-    /// @param copy The Expected to copy
+    /// @param copy The Expect to copy
     /// @return Self
-    constexpr Expected& operator=(const Expected& copy)
+    constexpr Expect& operator=(const Expect& copy)
       noexcept(std::is_nothrow_copy_constructible_v<ExpectedTy>
         && std::is_nothrow_copy_constructible_v<ErrorTy>
         && std::is_nothrow_destructible_v<ErrorTy>
@@ -163,9 +163,9 @@ namespace clt
     }
     COLT_POST()
 
-    /// @brief Move constructs an Expected
-    /// @param move The Expected to move
-    constexpr Expected(Expected&& move)
+    /// @brief Move constructs an Expect
+    /// @param move The Expect to move
+    constexpr Expect(Expect&& move)
       noexcept(std::is_nothrow_move_constructible_v<ExpectedTy>
         && std::is_nothrow_move_constructible_v<ErrorTy>)
       : is_error_v(move.is_error_v)
@@ -177,9 +177,9 @@ namespace clt
     }
 
     /// @brief Move assignment operator
-    /// @param move The Expected to move
+    /// @param move The Expect to move
     /// @return Self
-    constexpr Expected& operator=(Expected&& move)
+    constexpr Expect& operator=(Expect&& move)
       noexcept(std::is_nothrow_move_constructible_v<ExpectedTy>
         && std::is_nothrow_move_constructible_v<ErrorTy>
         && std::is_nothrow_destructible_v<ErrorTy>
@@ -201,8 +201,8 @@ namespace clt
     }
     COLT_POST()
 
-    /// @brief Destructs the value/error contained in the Expected
-    constexpr ~Expected() noexcept(std::is_nothrow_destructible_v<ExpectedTy>
+    /// @brief Destructs the value/error contained in the Expect
+    constexpr ~Expect() noexcept(std::is_nothrow_destructible_v<ExpectedTy>
       && std::is_nothrow_destructible_v<ErrorTy>)
     {
       if (is_error_v)
@@ -211,46 +211,46 @@ namespace clt
         expected.~ExpectedTy();
     }
 
-    /// @brief Check if the Expected contains an error
-    /// @return True if the Expected contains an error
+    /// @brief Check if the Expect contains an error
+    /// @return True if the Expect contains an error
     constexpr bool is_error() const noexcept { return is_error_v; }
-    /// @brief Check if the Expected contains an expected value
-    /// @return True if the Expected contains an expected value
+    /// @brief Check if the Expect contains an expected value
+    /// @return True if the Expect contains an expected value
     constexpr bool is_expect() const noexcept { return !is_error_v; }
 
-    /// @brief Check if the Expected contains an error.
+    /// @brief Check if the Expect contains an error.
     /// Same as is_error().
-    /// @return True if the Expected contains an error
+    /// @return True if the Expect contains an error
     constexpr bool operator!() const noexcept { return is_error_v; }
-    /// @brief Check if the Expected contains an expected value.
+    /// @brief Check if the Expect contains an expected value.
     /// Same as is_expected().
-    /// @return True if the Expected contains an expected value
+    /// @return True if the Expect contains an expected value
     explicit constexpr operator bool() const noexcept { return !is_error_v; }
 
-    /// @brief Returns the stored Expected value.
-    /// @return The Expected value
+    /// @brief Returns the stored Expect value.
+    /// @return The Expect value
     constexpr const ExpectedTy* operator->() const noexcept
       COLT_PRE(is_expect())
       return &expected;
     COLT_POST()
     
-    /// @brief Returns the stored Expected value.
-    /// @return The Expected value
+    /// @brief Returns the stored Expect value.
+    /// @return The Expect value
     constexpr ExpectedTy* operator->() noexcept
       COLT_PRE(is_expect())
       return &expected;
     COLT_POST()
 
-    /// @brief Returns the stored Expected value.
-    /// @return The Expected value.
+    /// @brief Returns the stored Expect value.
+    /// @return The Expect value.
     constexpr meta::copy_trivial_t<const ExpectedTy&> operator*()
       const& noexcept
       COLT_PRE(is_expect())
       return expected;
     COLT_POST()
 
-    /// @brief Returns the stored Expected value.
-    /// @return The Expected value.
+    /// @brief Returns the stored Expect value.
+    /// @return The Expect value.
     /// @pre is_expected() (colt_expected_is_expected).
     constexpr ExpectedTy& operator*()
       & noexcept
@@ -258,48 +258,48 @@ namespace clt
       return expected;
     COLT_POST()
     
-    /// @brief Returns the stored Expected value.
-    /// @return The Expected value.
+    /// @brief Returns the stored Expect value.
+    /// @return The Expect value.
     constexpr meta::copy_trivial_t<const ExpectedTy&&> operator*()
       const&& noexcept
       COLT_PRE(is_expect())
       return expected;
     COLT_POST()
     
-    /// @brief Returns the stored Expected value.
-    /// @return The Expected value.
+    /// @brief Returns the stored Expect value.
+    /// @return The Expect value.
     constexpr ExpectedTy&& operator*()
       && noexcept
       COLT_PRE(is_expect())
       return std::move(expected);
     COLT_POST()
 
-    /// @brief Returns the stored Expected value.
-    /// @return The Expected value.
+    /// @brief Returns the stored Expect value.
+    /// @return The Expect value.
     constexpr meta::copy_trivial_t<const ExpectedTy&> value()
       const& noexcept
       COLT_PRE(is_expect())
       return expected;
     COLT_POST()
 
-    /// @brief Returns the stored Expected value.
-    /// @return The Expected value.
+    /// @brief Returns the stored Expect value.
+    /// @return The Expect value.
     constexpr ExpectedTy& value()
       & noexcept
       COLT_PRE(is_expect())
       return expected;
     COLT_POST()
     
-    /// @brief Returns the stored Expected value.
-    /// @return The Expected value.
+    /// @brief Returns the stored Expect value.
+    /// @return The Expect value.
     constexpr meta::copy_trivial_t<const ExpectedTy&&> value()
       const&& noexcept
       COLT_PRE(is_expect())
       return expected;
     COLT_POST()
     
-    /// @brief Returns the stored Expected value.
-    /// @return The Expected value.
+    /// @brief Returns the stored Expect value.
+    /// @return The Expect value.
     constexpr ExpectedTy&& value()
       && noexcept
       COLT_PRE(is_expect())
@@ -338,16 +338,16 @@ namespace clt
       return std::move(error_v);
     COLT_POST()
 
-    /// @brief Returns the Expected value if contained, else 'default_value'
-    /// @param default_value The value to return if the Expected contains an error
-    /// @return The Expected value or 'default_value'
+    /// @brief Returns the Expect value if contained, else 'default_value'
+    /// @param default_value The value to return if the Expect contains an error
+    /// @return The Expect value or 'default_value'
     constexpr ExpectedTy value_or(ExpectedTy && default_value) const&
     {
       return is_error_v ? static_cast<ExpectedTy>(std::forward<ExpectedTy>(default_value)) : **this;
     }
-    /// @brief Returns the Expected value if contained, else 'default_value'
-    /// @param default_value The value to return if the Expected contains an error
-    /// @return The Expected value or 'default_value'
+    /// @brief Returns the Expect value if contained, else 'default_value'
+    /// @param default_value The value to return if the Expect contains an error
+    /// @return The Expect value or 'default_value'
     constexpr ExpectedTy value_or(ExpectedTy&& default_value)&&
     {
       return is_error_v ? static_cast<ExpectedTy>(std::forward<ExpectedTy>(default_value)) : std::move(**this);
