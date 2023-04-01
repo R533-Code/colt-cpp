2c3b46f9b2a1c70c5ffe2dac1dbe97cce626abaf
R533-Code <raphaeldibnehmepc@gmail.com>
2023-04-01 19:53:18 +0300

    Renamed and added new methods

--- include/structs/option.h
+++ include/structs/option.h
@@ -1,6 +1,6 @@
-/** @file Optional.h
-* Contains the Optional class.
-* Optional<T> can be either a value or None.
+/** @file Option.h
+* Contains the Option class.
+* Option<T> can be either a value or None.
 */
 
 #ifndef HG_COLT_OPTIONAL
@@ -13,7 +13,7 @@ namespace clt
   template<typename T>
   /// @brief Manages an optionally contained value.
   /// @tparam T The optional type to hold
-  class Optional
+  class Option
   {
     /// @brief Buffer for the optional object
     alignas(T) char opt_buffer[sizeof(T)];
@@ -21,7 +21,7 @@ namespace clt
     bool is_none_v;
 
   public:
-    /// @brief Destroy the stored value if it exists, and sets the Optional to an empty one.
+    /// @brief Destroy the stored value if it exists, and sets the Option to an empty one.
     /// Called automatically by the destructor.
     constexpr void reset()
       noexcept(std::is_nothrow_destructible_v<T>)
@@ -33,28 +33,28 @@ namespace clt
       }
     }
 
-    /// @brief Constructs an empty Optional.
-    constexpr Optional() noexcept
+    /// @brief Constructs an empty Option.
+    constexpr Option() noexcept
       : is_none_v(true) {}
 
-    /// @brief Constructs an empty Optional.
-    /// Same as Optional().
+    /// @brief Constructs an empty Option.
+    /// Same as Option().
     /// @param  NoneT: use None
-    constexpr Optional(meta::NoneT) noexcept
+    constexpr Option(meta::NoneT) noexcept
       : is_none_v(true) {}
 
-    /// @brief Copy constructs an object into the Optional.
+    /// @brief Copy constructs an object into the Option.
     /// @param to_copy The object to copy
-    constexpr Optional(meta::copy_trivial_t<const T&> to_copy)
+    constexpr Option(meta::copy_trivial_t<const T&> to_copy)
       noexcept(std::is_nothrow_copy_constructible_v<T>)
       : is_none_v(false)
     {
       new(opt_buffer) T(to_copy);
     }
 
-    /// @brief Move constructs an object into the Optional
+    /// @brief Move constructs an object into the Option
     /// @param to_move The object to move
-    constexpr Optional(T&& to_move)
+    constexpr Option(T&& to_move)
       noexcept(std::is_nothrow_move_constructible_v<T>)
       requires (!std::is_trivial_v<T>)
     : is_none_v(false)
@@ -63,11 +63,11 @@ namespace clt
     }
 
     template<typename... Args>
-    /// @brief Constructs an object into the Optional directly.
+    /// @brief Constructs an object into the Option directly.
     /// @tparam ...Args The parameter pack
     /// @param  InPlaceT, use InPlace
     /// @param ...args The argument pack
-    constexpr Optional(meta::InPlaceT, Args&&... args)
+    constexpr Option(meta::InPlaceT, Args&&... args)
       noexcept(std::is_nothrow_constructible_v<T, Args...>)
       : is_none_v(false)
     {
@@ -75,8 +75,8 @@ namespace clt
     }
 
     /// @brief Copy constructor.
-    /// @param to_copy The Optional to copy
-    constexpr Optional(const Optional<T>& to_copy)
+    /// @param to_copy The Option to copy
+    constexpr Option(const Option& to_copy)
       noexcept(std::is_nothrow_copy_constructible_v<T>)
       : is_none_v(to_copy.is_none_v)
     {
@@ -85,8 +85,8 @@ namespace clt
     }
 
     /// @brief Move constructor.
-    /// @param to_move The Optional to move
-    constexpr Optional(Optional<T>&& to_move)
+    /// @param to_move The Option to move
+    constexpr Option(Option&& to_move)
       noexcept(std::is_nothrow_move_constructible_v<T>)
       : is_none_v(to_move.is_none_v)
     {
@@ -94,35 +94,53 @@ namespace clt
         new(opt_buffer) T(std::move(*details::ptr_to<T*>(to_move.opt_buffer)));
     }
 
-    /// @brief Resets the Optional.
+    constexpr Option& operator=(const Option& to_copy)
+      noexcept(std::is_nothrow_destructible_v<T>
+        && std::is_nothrow_copy_constructible_v<T>)
+    {
+      reset();
+      if (to_copy.is_value())
+        new(opt_buffer) T(*details::ptr_to<const T*>(to_copy.opt_buffer));
+    }
+
+    constexpr Option& operator=(Option&& to_move)
+      noexcept(std::is_nothrow_destructible_v<T>
+        && std::is_nothrow_move_constructible_v<T>)
+    {
+      reset();
+      if (to_move.is_value())
+        new(opt_buffer) T(std::move(*details::ptr_to<T*>(to_move.opt_buffer)));
+    }
+
+    /// @brief Resets the Option.
     /// Same as `reset()`.
     /// @param  Error type (Error)
     /// @return Reference to self
-    constexpr Optional& operator=(meta::NoneT)
+    constexpr Option& operator=(meta::NoneT)
       noexcept(std::is_nothrow_destructible_v<T>)
     {
       reset();
     }
 
     /// @brief Destructor, destructs the value if it exist.
-    constexpr ~Optional()
+    constexpr ~Option()
       noexcept(std::is_nothrow_destructible_v<T>)
     {
       reset()
     }
 
-    /// @brief Check if the Optional contains a value.
-    /// @return True if the Optional contains a value
+    /// @brief Check if the Option contains a value.
+    /// @return True if the Option contains a value
     explicit constexpr operator bool() const noexcept { return !is_none_v; }
 
-    /// @brief Check if the Optional contains a value.
+    /// @brief Check if the Option contains a value.
     /// Same as !is_none().
-    /// @return True if the Optional contains a value
+    /// @return True if the Option contains a value
     constexpr bool is_value() const noexcept { return !is_none_v; }
 
-    /// @brief Check if the Optional does not contain a value.
+    /// @brief Check if the Option does not contain a value.
     /// Same as !is_value().
-    /// @return True if the Optional does not contain a value
+    /// @return True if the Option does not contain a value
     constexpr bool is_none() const noexcept { return is_none_v; }
 
     /// @brief Returns the stored value.
@@ -198,7 +216,7 @@ namespace clt
     COLT_POST()
 
     /// @brief Returns the value if contained, else 'default_value'
-    /// @param default_value The value to return if the Optional is None
+    /// @param default_value The value to return if the Option is None
     /// @return The value or 'default_value'
     constexpr T value_or(T&& default_value) const&
     {
@@ -206,7 +224,7 @@ namespace clt
     }
     
     /// @brief Returns the value if contained, else 'default_value'
-    /// @param default_value The value to return if the Optional is None
+    /// @param default_value The value to return if the Option is None
     /// @return The value or 'default_value'
     constexpr T value_or(T&& default_value)&&
     {
