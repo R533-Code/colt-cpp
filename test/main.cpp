16963f660713258bcdd2406dac7f0834232ff355
R533-Code <raphaeldibnehmepc@gmail.com>
2023-04-01 20:18:08 +0300

    Tested Option

--- test/main.cpp
+++ test/main.cpp
@@ -4,6 +4,7 @@
 #include "io/input.h"
 #include "structs/vector.h"
 #include "structs/expect.h"
+#include "structs/option.h"
 
 using namespace std::chrono_literals;
 using namespace clt;
@@ -17,6 +18,13 @@ Expect<int, const char*> div_expect(int a, int b)
   return { Error, "Division by zero is prohibited!" };
 }
 
+Option<int> div_option(int a, int b)
+{
+  if (b != 0)
+    return a / b;
+  return { None };
+}
+
 int main(int argc, int argv)
 {
   std::atexit([]() { clt::bench::save_tracing_to("Test.json"); });
@@ -25,10 +33,12 @@ int main(int argc, int argv)
     COLT_PROFILE_SCOPE("fmt::print");
     print_message("Hello {}", "World!");
   }
+
   auto vec = Vector<int>(10ULL);
   vec.push_back(10);
   vec.push_back(13);
   vec.push_back(15);
   vec.pop_back_n(2);
   print("{}, {}", div_expect(10, 2), div_expect(19, 0));
+  print("{:test}, {:test}", div_option(10, 2), div_option(19, 0));
 }
