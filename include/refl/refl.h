b88c59f31a4d3b9ceca73b28a0be001c9ad85522
R533-Code <raphaeldibnehmepc@gmail.com>
2023-04-02 11:47:50 +0300

    Added refl struct

---
+++ include/refl/refl.h
@@ -0,0 +1,12 @@
+#ifndef HG_COLT_REFL
+#define HG_COLT_REFL
+
+namespace clt
+{
+  template<typename T>
+  /// @brief Unspecialized reflection informations
+  /// @tparam T The type on which to reflect
+  struct refl{};
+}
+
+#endif //!HG_COLT_REFL
