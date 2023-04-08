a921731f107e04d88ae5d516ef89487dbfc96526
R533-Code <raphaeldibnehmepc@gmail.com>
2023-04-08 12:38:05 +0300

    Added more helpers to type_list

--- include/meta/type_list.h
+++ include/meta/type_list.h
@@ -2,10 +2,18 @@
 #define HG_COLT_TYPE_LIST
 
 #include <cstdint>
+#include <tuple>
+#include <type_traits>
 
 namespace clt::meta
 {
-	template<typename... Ts>
+  template<typename T>
+  /// @brief Helper condition to give to static_assert to avoid compilation error
+  /// before template is not instantiated.
+  /// @tparam T Any type
+  inline constexpr bool always_false = false;
+
+  template<typename... Ts>
   // forward declaration
   struct type_list;
 
@@ -54,11 +62,41 @@ namespace clt::meta
     {};
   }
 
+  namespace details
+  {
+    template<typename...Ts>
+    using tuple_cat_t = decltype(std::tuple_cat(std::declval<Ts>()...));
+
+    template<typename T, typename...Ts>
+    using remove_t = tuple_cat_t<
+      typename std::conditional_t<
+      std::is_same_v<T, Ts>,
+      std::tuple<>,
+      std::tuple<Ts>
+      >...
+    >;
+
+    template<typename... Ts>
+    type_list<Ts...> from_tuple_to_type_list(std::tuple<Ts...>) noexcept
+    {
+      static_assert(always_false<void>,
+        "from_tuple_type_list is not allowed in an evaluated context!");
+    }
+  }
+
+  template<typename T>
+  /// @brief Converts a tuple to a type list
+  /// @tparam T The tuple type
+  using tuple_to_type_list_t = decltype(details::from_tuple_to_type_list(std::declval<T>()));
+
   template<typename... Ts>
   /// @brief List of types
   /// @tparam ...Ts The types to hold
   struct type_list
   {
+    /// @brief The type of the current list (not very useful)
+    using this_list = type_list<Ts...>;
+
     template<uint64_t index> requires (index < sizeof...(Ts))
     /// @brief Returns the type at index 'index'
     using get = typename details::get_n<index, Ts...>::type;
@@ -74,7 +112,8 @@ namespace clt::meta
     using push_front = type_list<T, Ts...>;
 
     template<template<typename> typename apply_f>
-    /// @brief Applies a transformation to the list
+    /// @brief Applies a transformation to the list.
+    /// To avoid compiler error, write "template" before accessing this member
     using apply = type_list<typename apply_f<Ts>::type...>;
 
     /// @brief Returns the head of the list
@@ -84,6 +123,29 @@ namespace clt::meta
 
     /// @brief Size of the list
     static constexpr size_t size = sizeof...(Ts);
+
+    /// @brief Returns the head of the list or void if the list is empty
+    using head_or_void = std::conditional_t<size == 0, void, get<0>>;
+    /// @brief Returns the tail of the list or void if the list is empty
+    using tail_or_void = std::conditional_t<size == 0, void, get<size - 1>>;
+
+    template<typename T, template<typename> condition>
+    /// @brief Pushes a type to the front of the list if 'condition<T>::value' is true
+    /// @tparam T The type to push
+    using push_front_if = std::conditional_t<condition<T>::value, push_front<T>, this_list>;
+
+    template<typename T, template<typename> condition>
+    /// @brief Pushes a type to the back of the list if 'condition<T>::value' is true
+    /// @tparam T The type to push
+    using push_back_if = std::conditional_t<condition<T>::value, push_back<T>, this_list>;
+
+    template<typename What>
+    /// @brief Remove all types that are the same as 'What'
+    /// @tparam What The type to remove
+    using remove_all = tuple_to_type_list_t<details::remove_t<What, Ts...>>;
+    
+    /// @brief Removes all void types from the type list
+    using pop_void = remove_all<void>;
   };
 }
 
