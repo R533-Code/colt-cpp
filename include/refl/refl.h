310a05ec0b254bb8fd54cc2f446a73a5e76ab061
R533-Code <raphaeldibnehmepc@gmail.com>
2023-04-10 12:45:41 +0300

    Added reflection for references and custom types

--- include/refl/refl.h
+++ include/refl/refl.h
@@ -4,6 +4,7 @@
 #include "../meta/string_literal.h"
 #include "../meta/traits.h"
 #include "../meta/type_list.h"
+#include "../util/macro.h"
 #include "../structs/string_view.h"
 
 namespace clt
@@ -73,6 +74,60 @@ namespace clt
     static constexpr void apply_on_methods(On&& obj, F&& fn)
     {
       //Pointers do not have methods... Do nothing.
+    }
+  };
+
+  template<typename T> requires std::is_lvalue_reference_v<T>
+    && meta::Reflectable<std::remove_reference_t<T>>
+    && (!std::is_const_v<T>) && (!std::is_volatile_v<T>)
+  /// @brief Pointer overload for reflection information
+  /// @tparam T The pointer type
+  struct refl<T>
+  {
+  private:
+    /// @brief Type name
+    static constexpr StringView name_str = refl<std::remove_reference_t<T>>::str();
+    /// @brief Helper string for generating type name
+    static constexpr StringView end_str = "&";
+
+  public:
+    /// @brief Returns the qualified type name
+    /// @return StringView over the qualified type name
+    static constexpr clt::StringView str() noexcept
+    {
+      return meta::join_strv_v<name_str, end_str>;
+    }
+
+  public:
+    /// @brief The unqualified reflected type
+    using unqualified_refl = refl<std::remove_reference_t<std::remove_cv_t<T>>>;
+    /// @brief The only member is the pointer itself
+    using members_type = typename meta::type_list<std::remove_reference_t<T>>::template apply<std::add_lvalue_reference>;
+
+    template<typename On, typename F> requires std::same_as<On, std::decay_t<T>>
+    /// @brief Applies a lambda on each member of an object
+    /// @tparam On The object type
+    /// @tparam F The lambda type
+    /// @param obj The object
+    /// @param fn The lambda
+    static constexpr void apply_on_members(On&& obj, F&& fn)
+    {
+      unqualified_refl::apply_on_members(
+        std::forward<On>(obj), std::forward<F>(fn)
+      );
+    }
+
+    template<typename On, typename F> requires std::same_as<On, std::decay_t<T>>
+    /// @brief Applies a lambda on each method of an object
+    /// @tparam On The object type
+    /// @tparam F The lambda type
+    /// @param obj The object
+    /// @param fn The lambda
+    static constexpr void apply_on_methods(On&& obj, F&& fn)
+    {
+      unqualified_refl::apply_on_methods(
+        std::forward<On>(obj), std::forward<F>(fn)
+      );
     }
   };
 
@@ -280,7 +335,7 @@ namespace clt
   /// @param f The lambda function
   constexpr void for_each(meta::MembersT, T&& of, F&& f) noexcept
   {
-    refl<std::decay_t<T>>::apply_for_members(std::forward<T>(of), std::forward<F>(f));
+    refl<std::decay_t<T>>::apply_on_members(std::forward<T>(of), std::forward<F>(f));
   }
 
   template<typename T, typename F> requires meta::Reflectable<T>
@@ -292,16 +347,18 @@ namespace clt
   /// @param f The lambda function
   constexpr void for_each(meta::MethodsT, T&& of, F&& f) noexcept
   {
-    refl<std::decay_t<T>>::apply_for_methods(std::forward<T>(of), std::forward<F>(f));
+    refl<std::decay_t<T>>::apply_on_methods(std::forward<T>(of), std::forward<F>(f));
   }
 }
 
+/// @brief Declares reflection informations for 'type'
 #define DECLARE_BUILTIN_TYPE(type) \
   template<> \
   struct clt::refl<type> { \
     static constexpr clt::StringView str() noexcept { return #type; } \
     using unqualified_refl = refl<std::remove_cv_t<type>>; \
     using members_type = clt::meta::type_list<type>; \
+    using methods_type = clt::meta::type_list<>; \
     template<typename On, typename F> requires std::same_as<On, std::decay_t<type>>\
     static constexpr void apply_on_members(On&& obj, F&& fn) { \
         fn(obj); \
@@ -310,6 +367,10 @@ namespace clt
     static constexpr void apply_on_methods(On&& obj, F&& fn) { \
     } \
   }
+
+/***************************
+* Declare all built-in types
+****************************/
 
 DECLARE_BUILTIN_TYPE(char);
 DECLARE_BUILTIN_TYPE(char8);
@@ -326,14 +387,31 @@ DECLARE_BUILTIN_TYPE(u64);
 DECLARE_BUILTIN_TYPE(f32);
 DECLARE_BUILTIN_TYPE(f64);
 
+/// @brief We no longer need this macro
 #undef DECLARE_BUILTIN_TYPE
 
-#define COLT_DECLARE_TYPE(type, ...) \
+#define COLT_DETAILS_MEMBER_TO_MEMBER_PTR(type, name) , decltype(&type::name)
+#define COLT_DETAILS_IF_CONSTEXPR_IS_MEMBER_CALL_LAMBDA(TYPE, member) if constexpr (std::is_member_object_pointer_v<decltype(&TYPE::member)>) fn(obj.member);
+
+#define COLT_DECLARE_TYPE(TYPE, member, ...) \
   template<> \
-  struct clt::refl<type> { \
+  struct clt::refl<TYPE> { \
+    static constexpr clt::StringView str() noexcept { return #TYPE; } \
+    using unqualified_refl = refl<std::remove_cv_t<TYPE>>; \
+    using members_type = typename clt::meta::type_list<decltype(&TYPE::member) COLT_FOR_EACH_1ARG(COLT_DETAILS_MEMBER_TO_MEMBER_PTR, TYPE, __VA_ARGS__)> \
+      ::template remove_if<std::is_member_function_pointer>; \
+    using methods_type = typename clt::meta::type_list<decltype(&TYPE::member) COLT_FOR_EACH_1ARG(COLT_DETAILS_MEMBER_TO_MEMBER_PTR, TYPE, __VA_ARGS__)> \
+      ::template remove_if<std::is_member_object_pointer>; \
+    template<typename On, typename F> requires std::same_as<std::decay_t<On>, std::decay_t<TYPE>>\
+    static constexpr void apply_on_members(On&& obj, F&& fn) { \
+        if constexpr (std::is_member_object_pointer_v<decltype(&TYPE::member)>) \
+          fn(obj.member); \
+        COLT_FOR_EACH_1ARG(COLT_DETAILS_IF_CONSTEXPR_IS_MEMBER_CALL_LAMBDA, TYPE, __VA_ARGS__) \
+    } \
   }
 
 template<typename T> requires (!fmt::is_formattable<T>::value) && clt::meta::Reflectable<T>
+/// @brief Format any type whose "Reflectable" and not already formattable.
 struct fmt::formatter<T>
 {
   template<typename ParseContext>
@@ -350,15 +428,15 @@ struct fmt::formatter<T>
   {
     auto out = ctx.out();
     out = fmt::format_to(out, "{{\n");
-    /*clt::for_each(clt::Members, obj,
-      [&out, i = 0ULL]<typename T>(T&& a) mutable
+    clt::for_each(clt::Members, obj,
+      [&out]<typename Ty>(Ty&& a)
       {
         out = fmt::format_to(out, "  {}: {}\n",
-          clt::refl<std::decay_t<T>>::str(), std::forward<T>(a));
+          clt::refl<std::decay_t<Ty>>::str(), std::forward<Ty>(a));
       }
-    );*/
+    );
     out = fmt::format_to(out, "}}\n");
-    return ;
+    return out;
   }
 };
 
