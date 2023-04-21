bdc14ef25d53a0636c3e60283fddd8fbe2267aaa
R533-Code <raphaeldibnehmepc@gmail.com>
2023-04-21 17:35:17 +0300

    Improved ConstexprMap

--- include/meta/map.h
+++ include/meta/map.h
@@ -17,15 +17,32 @@ namespace clt::meta
     /// @brief The data in which to search
     std::array<std::pair<Key, Value>, Size> data;
 
+    constexpr ConstexprMap(std::array<std::pair<Key, Value>, Size> data)
+      : data(data)
+    {
+      std::sort(data.begin(), data.end());
+      assert_true("Items not unique!",
+        std::adjacent_find(data.begin(), data.end()) == data.end());
+    }
+
     [[nodiscard]]
     /// @brief Searches for the value associated with 'key'
     /// @param key The key whose value to find
     /// @return None if not found, else the value
     constexpr Option<Value> find(const Key& key) const
-    {      
-      if (const auto itr = std::find_if(begin(data), end(data), [&key](const auto& v) { return v.first == key; });
-        itr != std::end(data))
-        return itr->second;
+    {
+      u64 L = 0;
+      u64 R = Size - 1;
+      while (L <= R)
+      {
+        u64 M = std::midpoint(L, R);
+        if (data[M].first < key)
+          L = M + 1;
+        else if (data[M].first > key)
+          R = M - 1;
+        else
+          return data[M].second;
+      }
       return None;
     }
 
