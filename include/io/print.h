d2cef97f4af0ef086355b49ee2faf40e2831e5df
R533-Code <raphaeldibnehmepc@gmail.com>
2023-04-23 19:59:23 +0300

    Added colored output for print_*

--- include/io/print.h
+++ include/io/print.h
@@ -7,11 +7,7 @@
 
 #include <utility>
 
-#ifndef FMT_HEADER_ONLY
-  #define FMT_HEADER_ONLY
-#endif //!FMT_HEADER_ONLY
-#include "../libraries/fmt/include/fmt/format-inl.h"
-
+#include "./color.h"
 #include "../meta/string_literal.h"
 #include "../util/typedefs.h"
 
@@ -52,7 +48,7 @@ namespace clt::io
   constexpr void print_error(fmt_str<Args...> fmt, Args&&... args) noexcept
   {
     fmt::basic_memory_buffer<char, 4096> buffer;
-    fmt::format_to(std::back_inserter(buffer), "Error: ");
+    fmt::format_to(std::back_inserter(buffer), "{}Error:{} ", BrightRedF, Reset);
     fmt::format_to(std::back_inserter(buffer), fmt, std::forward<Args>(args)...);
     if constexpr (endl.size() != 0)
       fmt::format_to(std::back_inserter(buffer), "{}", fmt::string_view{ endl.value, endl.size() });
@@ -67,7 +63,7 @@ namespace clt::io
   constexpr void print_warn(fmt_str<Args...> fmt, Args&&... args) noexcept
   {
     fmt::basic_memory_buffer<char, 4096> buffer;
-    fmt::format_to(std::back_inserter(buffer), "Warning: ");
+    fmt::format_to(std::back_inserter(buffer), "{}Warning:{} ", YellowF, Reset);
     fmt::format_to(std::back_inserter(buffer), fmt, std::forward<Args>(args)...);
     if constexpr (endl.size() != 0)
       fmt::format_to(std::back_inserter(buffer), "{}", fmt::string_view{ endl.value, endl.size() });
@@ -82,7 +78,7 @@ namespace clt::io
   constexpr void print_message(fmt_str<Args...> fmt, Args&&... args) noexcept
   {
     fmt::basic_memory_buffer<char, 4096> buffer;
-    fmt::format_to(std::back_inserter(buffer), "Message: ");
+    fmt::format_to(std::back_inserter(buffer), "{}Message:{} ", CyanF, Reset);
     fmt::format_to(std::back_inserter(buffer), fmt, std::forward<Args>(args)...);
     if constexpr (endl.size() != 0)
       fmt::format_to(std::back_inserter(buffer), "{}", fmt::string_view{ endl.value, endl.size() });
@@ -97,7 +93,7 @@ namespace clt::io
   constexpr void print_fatal(fmt::format_string<Args...> fmt, Args && ...args)
   {
     fmt::basic_memory_buffer<char, 4096> buffer;
-    fmt::format_to(std::back_inserter(buffer), "FATAL: ");
+    fmt::format_to(std::back_inserter(buffer), "{}FATAL:{} {}", BrightRedB, Reset, BrightRedF);
     fmt::format_to(std::back_inserter(buffer), fmt, std::forward<Args>(args)...);
     if constexpr (endl.size() != 0)
       fmt::format_to(std::back_inserter(buffer), "{}", fmt::string_view{ endl.value, endl.size() });
