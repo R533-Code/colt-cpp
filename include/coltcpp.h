186b87eb0477c91846f61dbff895d69e70d03e8e
R533-Code <raphaeldibnehmepc@gmail.com>
2023-04-29 17:15:16 +0300

    Added new helpers

--- include/coltcpp.h
+++ include/coltcpp.h
@@ -37,6 +37,40 @@
 
 namespace clt
 {
+  template<typename Target, typename Input>
+  [[nodiscard]]
+  /// @brief Short-hand for static_cast<Target>(Input), with some debug checks
+  /// @tparam Target The resulting type
+  /// @tparam Input The type to convert from
+  /// @param input The value to convert
+  /// @return static_cast<Target>(input)
+  constexpr Target as(Input&& input)
+  {
+    if constexpr (is_debug())
+    {
+      // DYNAMIC CASTING CHECK
+      if constexpr (std::is_pointer_v<Target>
+        && std::is_pointer_v<std::decay_t<Input>>
+        && meta::DynCastableTo<std::remove_pointer_t<std::decay_t<Input>>, std::remove_pointer_t<std::decay_t<Target>>>)
+      {
+        if (is_a<Target>(input))
+          return static_cast<Target>(std::forward<Input>(input));
+        colt_unreachable("'as' conversion failed as true type did not match the expected one!");
+      }
+      // NUMERICAL TRUNCATION CHECK
+      else if constexpr (std::is_integral_v<Target>
+        && std::is_integral_v<Input>
+        && sizeof(Target) < sizeof(Input))
+      {
+        if (input > std::numeric_limits<Target>::max())
+          colt_unreachable("'as' conversion failed as resulting value would be truncated!");
+        else if (input < std::numeric_limits<Target>::min())
+          colt_unreachable("'as' conversion failed as resulting value would be truncated!");
+      }
+    }
+    return static_cast<Target>(std::forward<Input>(input));
+  }
+
   void install_colt_handlers() noexcept
   {
 #ifndef COLT_NO_PROFILE
