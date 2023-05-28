001f51ed0a3f6e1b19d190f9124c056db217ab6d
R533-Code <raphaeldibnehmepc@gmail.com>
2023-05-28 22:27:15 +0300

    Revamped input function and made use of scn

--- include/io/input.h
+++ include/io/input.h
@@ -21,6 +21,16 @@
 #include "../refl/enum.h"
 #include "../structs/string.h"
 
+DECLARE_ENUM_WITH_TYPE(u8, clt, ParsingResult,
+  GOOD,             //no errors
+  FILE_EOF,         //EOF detected
+  FILE_ERROR,       //error reading from file
+  INVALID_ENCODING, //invalid characters encountered (non-ASCII)
+  INVALID_VALUE,    //invalid value to parse
+  OUT_OF_RANGE,     //value cannot be stored in type
+  NON_EMPTY_REM     //not all characters were consumed
+  );
+
 namespace clt::io
 {
   inline void toggle_echo() noexcept
@@ -44,6 +54,28 @@ namespace clt::io
 #endif
   }
 
+  namespace details
+  {
+    constexpr ParsingResult scn_error_to_parse(scn::error code) noexcept
+    {
+      switch (code.code())
+      {
+      case scn::error::good:
+        return ParsingResult::GOOD;
+      case scn::error::end_of_range:
+        return ParsingResult::FILE_EOF;
+      case scn::error::invalid_scanned_value:
+        return ParsingResult::INVALID_VALUE;
+      case scn::error::value_out_of_range:
+        return ParsingResult::OUT_OF_RANGE;
+      case scn::error::invalid_encoding:
+        return ParsingResult::INVALID_ENCODING;
+      default:
+        colt_unreachable("Unknown error!");
+      }
+    }
+  }
+
   /// @brief Prints 'Press any key to continue...' and waits for any key input.
   inline void press_to_continue() noexcept
   {
@@ -52,107 +84,32 @@ namespace clt::io
     (void)getchar();
     toggle_echo();
     std::fputc('\n', stdout);
-  }
+  }  
 
-  
-  inline Expect<u32, IOError> gets_no_echo(clt::Span<char> span) noexcept
-    COLT_PRE(!span.is_empty())
-  {
-    auto n = static_cast<u32>(span.size());
-    char* buffer = span.data();
-    
-    toggle_echo();
-    int chr = getchar();
-    if (chr == EOF)
-      return { Error, IOError::FILE_EOF };
-    buffer[0] = chr;
-    u32 i = 1;
-    for (; i < n; i++)
-    {
-      chr = getchar();
-      if (chr == EOF || chr == '\n')
-        break;
-      buffer[i] = static_cast<char>(chr);
-    }
-    toggle_echo();
-    return { i };
-  }
-  COLT_POST()
-
-  inline Expect<u32, IOError> gets(Span<char> span) noexcept
-    COLT_PRE(!span.is_empty())
-  {
-    auto n = static_cast<u32>(span.size());
-    char* buffer = span.data();
-
-    int chr = getchar();
-    if (chr == EOF)
-    {
-      if (feof(stdin))
-        return { Error, IOError::FILE_EOF };
-      else
-        return { Error, IOError::FILE_ERROR };
-    }
-    buffer[0] = static_cast<char>(chr);
-    u32 i = 1;
-    for (; i < n; i++)
-    {
-      chr = getchar();
-      if (chr == EOF || chr == '\n')
-        break;
-      buffer[i] = static_cast<char>(chr);
-    }
-    return { i };
-  }
-  COLT_POST()
-
-  template<meta::Inputable T = String, meta::StringLiteral endl = "", typename... Args>
-  inline Expect<T, IOError> input(std::FILE* file, fmt_str<Args...> fmt, Args&&... args) noexcept
+  template<typename T = String, typename... Args>
+  inline Expect<T, ParsingResult> input(std::FILE* file, fmt_str<Args...> fmt, Args&&... args) noexcept
   {
     //Print the message...
-    print<endl>(fmt, std::forward<Args>(args)...);
+    print<"">(fmt, std::forward<Args>(args)...);
     //Ask for input...
-    auto str = String::getLine(file, str::recommended_size<T>(),
-      !meta::NoStripParsable<T>);
-    if (str.is_error()) //FILE_EOF or FILE_ERROR
-      return { Error, str.error() };
-    
-    //The string to parse.
-    //As String::getLine strips the spaces from the front,
-    //we only strip the spaces in the back.
-    auto strv = StringView{ str->begin(), str->end() };
-    if constexpr (!meta::NoStripParsable<T>)
-      strv.strip_suffix();
-    
-    //The variable in which to store the result
-    uninit<T> result;
-    
-    //Parse the line
-    auto [ptr, err] = clt::str::parser<T>{}(result, strv);
-    if (err != ParseErrorCode::SUCCESS)
+    auto str = String::getLine(file);
+    if (str.is_error()) //FILE_EOF or FILE_ERROR or INVALID_ENCODING
+      return { Error, static_cast<ParsingResult>(static_cast<u8>(str.error()) + 1) };
+
+    T ret;
+    StringView strv = *str;
+    auto result = scn::scan_default(strv.strip(), ret);
+    if (result)
     {
-      if (err == ParseErrorCode::INVALID_FMT)
-        return { Error, IOError::INVALID_FMT };
-      else if (err == ParseErrorCode::OUT_OF_RANGE)
-        return { Error, IOError::OUT_OF_RANGE };
-      //Check if a new IOError was added...
-      colt_unreachable("Invalid ParseErrorCode!");
+      if (!result.empty())
+        return { Error, ParsingResult::NON_EMPTY_REM };
+      return std::move(ret);
     }
-    //err == SUCCESS, the object was constructed,
-    //We need to destruct it.
-    ON_SCOPE_EXIT{
-      result.destruct();
-    };
-    
-    //There was more characters that did not match:
-    // "10 x" is not a valid integer as the " x" won't be accessible.
-    if (ptr != strv.end())
-      return { Error, IOError::INVALID_FMT };
-    return { InPlace, std::move(result.data()) };
+    return { Error, details::scn_error_to_parse(result.error()) };
   }
 
-  template<meta::Inputable T = String, meta::StringLiteral endl = "", typename... Args>
-  inline Expect<T, IOError> input(fmt_str<Args...> fmt, Args&&... args) noexcept
+  template<typename T = String, meta::StringLiteral endl = "", typename... Args>
+  inline Expect<T, ParsingResult> input(fmt_str<Args...> fmt, Args&&... args) noexcept
   {
     return input<T>(stdin, fmt, std::forward<Args>(args)...);
   }
