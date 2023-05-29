bc3b3e665b4300e4847b0f4978464e560bd6d867
R533-Code <raphaeldibnehmepc@gmail.com>
2023-05-29 23:54:16 +0300

    Added Parsable concept

--- include/str/parse.h
+++ include/str/parse.h
@@ -20,6 +20,9 @@ namespace clt::meta
 {
   template<Integral T>
   inline constexpr u64 max_digits10_v = static_cast<u64>(clt::ceil(clt::log10(std::numeric_limits<T>::max()))) + std::is_signed_v<T>;
+
+  template<typename T>
+  concept Parsable = requires { std::declval<scn::scanner<T>>().scan; };
 }
 
 DECLARE_ENUM_WITH_TYPE(u8, clt::io, IOError,
