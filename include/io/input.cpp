7a7506b4795f26a0041f4cd5e2c7450371fc9dda
R533-Code <raphaeldibnehmepc@gmail.com>
2023-05-29 21:58:30 +0300

    Moved function body to .cpp

---
+++ include/io/input.cpp
@@ -0,0 +1,33 @@
+#include "input.h"
+
+#ifndef _WIN32
+  #include <termios.h>
+  #include <unistd.h>
+#else
+#define NOMINMAX
+  #include <Windows.h>
+#endif //COLT_WINDOWS
+
+namespace clt::io
+{
+  void toggle_echo() noexcept
+  {
+#ifdef _WIN32
+    HANDLE h;
+    DWORD mode;
+
+    h = GetStdHandle(STD_INPUT_HANDLE);
+    if (GetConsoleMode(h, &mode))
+    {
+      mode &= ~ENABLE_ECHO_INPUT;
+      SetConsoleMode(h, mode);
+    }
+#else
+    struct termios ts;
+    tcgetattr(fileno(stdin), &ts);
+    ts.c_lflag &= ~ECHO;
+    tcsetattr(fileno(stdin), TCSANOW, &ts);
+
+#endif
+  }
+}
--- include/io/input.h
+++ include/io/input.h
@@ -5,14 +5,6 @@
 #ifndef HG_COLT_INPUT
 #define HG_COLT_INPUT
 
-#ifndef _WIN32
-  #include <termios.h>
-  #include <unistd.h>
-#else
-  #define NOMINMAX
-  #include <Windows.h>
-#endif //COLT_WINDOWS
-
 #include <utility>
 
 #include "../str/parse.h"
@@ -23,26 +15,8 @@
 
 namespace clt::io
 {
-  inline void toggle_echo() noexcept
-  {
-#ifdef _WIN32
-    HANDLE h;
-    DWORD mode;
-
-    h = GetStdHandle(STD_INPUT_HANDLE);
-    if (GetConsoleMode(h, &mode))
-    {
-      mode &= ~ENABLE_ECHO_INPUT;
-      SetConsoleMode(h, mode);
-    }
-#else
-    struct termios ts;
-    tcgetattr(fileno(stdin), &ts);
-    ts.c_lflag &= ~ECHO;
-    tcsetattr(fileno(stdin), TCSANOW, &ts);
-
-#endif
-  }
+  /// @brief Turns on/off echo to the console
+  void toggle_echo() noexcept;  
 
   /// @brief Prints 'Press any key to continue...' and waits for any key input.
   inline void press_to_continue() noexcept
