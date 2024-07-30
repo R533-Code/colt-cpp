776f760fba9b9fda7b3eb7613115f0412e5e781e
R533-Code <raphaeldibnehmepc@gmail.com>
2024-07-30 23:12:08 +0200

    Added example test

--- test/run_tests.cpp
+++ test/run_tests.cpp
@@ -1,2 +1,10 @@
 #define CONFIG_CATCH_MAIN
-#include <catch.hpp>
+#include <catch2/catch_all.hpp>
+
+// TEST_CASE("Factorials are computed", "[factorial]")
+// {
+//   REQUIRE(test_factorial(1) == 1);
+//   REQUIRE(test_factorial(2) == 2);
+//   REQUIRE(test_factorial(3) == 6);
+//   REQUIRE(test_factorial(10) == 3'628'800);
+// }
