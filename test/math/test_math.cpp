5a3162fc3b69a784e111eb48ebc8b2c67d32f95a
R533-Code <raphaeldibnehmepc@gmail.com>
2024-07-31 13:33:21 +0200

    Added new test

--- test/math/test_math.cpp
+++ test/math/test_math.cpp
@@ -24,3 +24,15 @@ TEST_CASE("Square Root", "[square_root]")
   REQUIRE(clt::math::sqrt(0.0) == 0.0);
   REQUIRE(clt::math::sqrt(16.0) == 4.0);
 }
+
+TEST_CASE("Power", "[power]")
+{
+  REQUIRE(clt::math::pow(1, 1) == 1);
+  REQUIRE(clt::math::pow(9, 0) == 1);
+
+  REQUIRE(clt::math::pow(2, 4) == 16);
+  REQUIRE(clt::math::pow(3, 3) == 27);
+
+  REQUIRE(clt::math::pow(4.0, 0.5) == 0.0);
+  REQUIRE(clt::math::pow(2.0, 8.0) == 256.0);
+}
