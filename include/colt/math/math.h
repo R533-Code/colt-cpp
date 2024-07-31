e5cd252d0f4e170529b9a9ca5dd551800ef11367
R533-Code <raphaeldibnehmepc@gmail.com>
2024-07-31 13:33:12 +0200

    Added more functions

--- include/colt/math/math.h
+++ include/colt/math/math.h
@@ -2,65 +2,105 @@
 #define HG_MATH_MATH
 
 #include <cmath>
+#include <numeric>
 #include <concepts>
+#include <algorithm>
+#include <utility>
 
 #include <gcem.hpp>
 
-#define MAKE_CONSTEXPR_MATH_FUNCTION(name, concept_name) \
-  template<concept_name T>                               \
-  constexpr auto name(T value) noexcept                  \
-  {                                                      \
-    if (std::is_constant_evaluated())                    \
-      return gcem::name(value);                          \
-    return std::name(value);                             \
+#define MAKE_CONSTEXPR_MATH_UNARY(name, concept_name) \
+  template<concept_name T>                            \
+  constexpr auto name(T value) noexcept               \
+  {                                                   \
+    if (std::is_constant_evaluated())                 \
+      return gcem::name(value);                       \
+    return std::name(value);                          \
+  }
+
+#define MAKE_CONSTEXPR_MATH_BINARY(name, concept_name) \
+  template<concept_name T>                             \
+  constexpr auto name(T first, T second) noexcept      \
+  {                                                    \
+    if (std::is_constant_evaluated())                  \
+      return gcem::name(first, second);                \
+    return std::name(first, second);                   \
   }
 
 namespace clt::math
 {
-  MAKE_CONSTEXPR_MATH_FUNCTION(sin, std::floating_point);
-  MAKE_CONSTEXPR_MATH_FUNCTION(asin, std::floating_point);
+  MAKE_CONSTEXPR_MATH_UNARY(sin, std::floating_point);
+  MAKE_CONSTEXPR_MATH_UNARY(asin, std::floating_point);
 
-  MAKE_CONSTEXPR_MATH_FUNCTION(sinh, std::floating_point);
-  MAKE_CONSTEXPR_MATH_FUNCTION(asinh, std::floating_point);
+  MAKE_CONSTEXPR_MATH_UNARY(sinh, std::floating_point);
+  MAKE_CONSTEXPR_MATH_UNARY(asinh, std::floating_point);
 
-  MAKE_CONSTEXPR_MATH_FUNCTION(cos, std::floating_point);
-  MAKE_CONSTEXPR_MATH_FUNCTION(acos, std::floating_point);
+  MAKE_CONSTEXPR_MATH_UNARY(cos, std::floating_point);
+  MAKE_CONSTEXPR_MATH_UNARY(acos, std::floating_point);
 
-  MAKE_CONSTEXPR_MATH_FUNCTION(cosh, std::floating_point);
-  MAKE_CONSTEXPR_MATH_FUNCTION(acosh, std::floating_point);
+  MAKE_CONSTEXPR_MATH_UNARY(cosh, std::floating_point);
+  MAKE_CONSTEXPR_MATH_UNARY(acosh, std::floating_point);
 
-  MAKE_CONSTEXPR_MATH_FUNCTION(tan, std::floating_point);
-  MAKE_CONSTEXPR_MATH_FUNCTION(atan, std::floating_point);
+  MAKE_CONSTEXPR_MATH_UNARY(tan, std::floating_point);
+  MAKE_CONSTEXPR_MATH_UNARY(atan, std::floating_point);
 
-  MAKE_CONSTEXPR_MATH_FUNCTION(tanh, std::floating_point);
-  MAKE_CONSTEXPR_MATH_FUNCTION(atanh, std::floating_point);
+  MAKE_CONSTEXPR_MATH_BINARY(atan2, std::floating_point);
 
-  MAKE_CONSTEXPR_MATH_FUNCTION(sqrt, std::floating_point);
-  MAKE_CONSTEXPR_MATH_FUNCTION(sqrt, std::integral);
+  MAKE_CONSTEXPR_MATH_UNARY(tanh, std::floating_point);
+  MAKE_CONSTEXPR_MATH_UNARY(atanh, std::floating_point);
 
-  MAKE_CONSTEXPR_MATH_FUNCTION(abs, std::floating_point);
-  MAKE_CONSTEXPR_MATH_FUNCTION(abs, std::integral);
+  MAKE_CONSTEXPR_MATH_UNARY(sqrt, std::floating_point);
+  MAKE_CONSTEXPR_MATH_UNARY(sqrt, std::integral);
 
-  MAKE_CONSTEXPR_MATH_FUNCTION(pow, std::floating_point);
-  MAKE_CONSTEXPR_MATH_FUNCTION(pow, std::integral);
+  MAKE_CONSTEXPR_MATH_UNARY(abs, std::floating_point);
+  MAKE_CONSTEXPR_MATH_UNARY(abs, std::integral);
 
-  MAKE_CONSTEXPR_MATH_FUNCTION(exp, std::floating_point);
-  MAKE_CONSTEXPR_MATH_FUNCTION(exp, std::integral);
+  MAKE_CONSTEXPR_MATH_BINARY(pow, std::floating_point);
+  MAKE_CONSTEXPR_MATH_BINARY(pow, std::integral);
 
-  MAKE_CONSTEXPR_MATH_FUNCTION(log, std::floating_point);
-  MAKE_CONSTEXPR_MATH_FUNCTION(log, std::integral);
+  MAKE_CONSTEXPR_MATH_UNARY(exp, std::floating_point);
+  MAKE_CONSTEXPR_MATH_UNARY(exp, std::integral);
 
-  MAKE_CONSTEXPR_MATH_FUNCTION(log10, std::floating_point);
-  MAKE_CONSTEXPR_MATH_FUNCTION(log10, std::integral);
+  MAKE_CONSTEXPR_MATH_UNARY(log, std::floating_point);
+  MAKE_CONSTEXPR_MATH_UNARY(log, std::integral);
 
-  MAKE_CONSTEXPR_MATH_FUNCTION(log2, std::floating_point);
-  MAKE_CONSTEXPR_MATH_FUNCTION(log2, std::integral);
+  MAKE_CONSTEXPR_MATH_UNARY(log10, std::floating_point);
+  MAKE_CONSTEXPR_MATH_UNARY(log10, std::integral);
 
-  MAKE_CONSTEXPR_MATH_FUNCTION(floor, std::floating_point);
-  MAKE_CONSTEXPR_MATH_FUNCTION(ceil, std::floating_point);
-  MAKE_CONSTEXPR_MATH_FUNCTION(round, std::floating_point);  
+  MAKE_CONSTEXPR_MATH_UNARY(log2, std::floating_point);
+  MAKE_CONSTEXPR_MATH_UNARY(log2, std::integral);
+
+  MAKE_CONSTEXPR_MATH_UNARY(floor, std::floating_point);
+  MAKE_CONSTEXPR_MATH_UNARY(ceil, std::floating_point);
+  MAKE_CONSTEXPR_MATH_UNARY(round, std::floating_point);
+  MAKE_CONSTEXPR_MATH_UNARY(trunc, std::floating_point);
+
+  MAKE_CONSTEXPR_MATH_BINARY(fmod, std::floating_point);
+  MAKE_CONSTEXPR_MATH_BINARY(hypot, std::floating_point);
+
+  MAKE_CONSTEXPR_MATH_BINARY(gcd, std::integral);
+  MAKE_CONSTEXPR_MATH_BINARY(lcm, std::integral);
+
+  MAKE_CONSTEXPR_MATH_BINARY(min, std::integral);
+  MAKE_CONSTEXPR_MATH_BINARY(max, std::integral);
+
+  MAKE_CONSTEXPR_MATH_BINARY(min, std::floating_point);
+  MAKE_CONSTEXPR_MATH_BINARY(max, std::floating_point);
+
+  template<typename T>
+  constexpr T min(std::initializer_list<T> ilist) noexcept
+  {
+    return std::min(ilist);
+  }
+
+  template<typename T>
+  constexpr T max(std::initializer_list<T> ilist) noexcept
+  {
+    return std::max(ilist);
+  }
 } // namespace clt::math
 
-#undef MAKE_CONSTEXPR_MATH_FUNCTION
+#undef MAKE_CONSTEXPR_MATH_UNARY
+#undef MAKE_CONSTEXPR_MATH_BINARY
 
 #endif //!HG_MATH_MATH
