06a920aaa27beeef4780b420ad072df7ad5c18e3
R533-Code <raphaeldibnehmepc@gmail.com>
2024-07-31 17:11:16 +0200

    Added typedefs

---
+++ include/colt/num/typedefs.h
@@ -0,0 +1,28 @@
+#ifndef HG_NUM_TYPEDEFS
+#define HG_NUM_TYPEDEFS
+
+#include <cstdint>
+
+/// @brief Signed 8-bit integer
+using i8 = int8_t;
+/// @brief Unsigned 8-bit integer
+using u8 = uint8_t;
+/// @brief Signed 16-bit integer
+using i16 = int16_t;
+/// @brief Unsigned 16-bit integer
+using u16 = uint16_t;
+/// @brief Signed 32-bit integer
+using i32 = int32_t;
+/// @brief Unsigned 32-bit integer
+using u32 = uint32_t;
+/// @brief Signed 64-bit integer
+using i64 = int64_t;
+/// @brief Unsigned 64-bit integer
+using u64 = uint64_t;
+
+/// @brief 32-bit float
+using f32 = float;
+/// @brief 64-bit float
+using f64 = double;
+
+#endif // !HG_NUM_TYPEDEFS
