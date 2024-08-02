9a3eb8eb290dafb20217b4d011ad827037e01235
R533-Code <raphaeldibnehmepc@gmail.com>
2024-08-02 22:02:09 +0200

    Added test for unicode

---
+++ test/dsa/test_unicode.cpp
@@ -0,0 +1,33 @@
+#include <catch2/catch_all.hpp>
+#include <colt/dsa/unicode.h>
+
+TEST_CASE("Unicode Length", "[strlen]")
+{
+  using namespace clt;
+  using enum StringEncoding;
+
+  SECTION("UTF8")
+  {
+    REQUIRE(strlen(u8"\u000D") == 1);
+    REQUIRE(strlen(u8"\u00b1") == 1);
+    REQUIRE(strlen(u8"\u03b1") == 1);
+    REQUIRE(strlen(u8"\u03BA\u1F79\u03C3\u03BC\u03B5") == 5);
+    REQUIRE(strlen(u8"\u0288\u0119\u0835\u0E34\u021B") == 5);
+  }
+  SECTION("UTF16")
+  {
+    REQUIRE(strlen(u"\u000D") == 1);
+    REQUIRE(strlen(u"\u00b1") == 1);
+    REQUIRE(strlen(u"\u03b1") == 1);
+    REQUIRE(strlen(u"\u03BA\u1F79\u03C3\u03BC\u03B5") == 5);
+    REQUIRE(strlen(u"\u0288\u0119\u0835\u0E34\u021B") == 5);
+  }
+  SECTION("UTF32")
+  {
+    REQUIRE(strlen(U"\u000D") == 1);
+    REQUIRE(strlen(U"\u00b1") == 1);
+    REQUIRE(strlen(U"\u03b1") == 1);
+    REQUIRE(strlen(U"\u03BA\u1F79\u03C3\u03BC\u03B5") == 5);
+    REQUIRE(strlen(U"\u0288\u0119\u0835\u0E34\u021B") == 5);
+  }
+}
