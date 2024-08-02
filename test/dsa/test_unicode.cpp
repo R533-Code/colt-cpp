28ecfb80e403ceaf074f8f7f413bfb2495905a7c
R533-Code <raphaeldibnehmepc@gmail.com>
2024-08-02 22:14:54 +0200

    Added more tests for unicode

--- test/dsa/test_unicode.cpp
+++ test/dsa/test_unicode.cpp
@@ -31,3 +31,38 @@ TEST_CASE("Unicode Length", "[strlen]")
     REQUIRE(strlen(U"\u0288\u0119\u0835\u0E34\u021B") == 5);
   }
 }
+
+TEST_CASE("Unicode Size", "[bytelen]")
+{
+  using namespace clt;
+  using enum StringEncoding;
+
+#define TEST_BYTELEN(value) bytelen(value) == (sizeof value - sizeof(value[0]))
+
+  SECTION("UTF8")
+  {
+    REQUIRE(TEST_BYTELEN(u8"\u000D"));
+    REQUIRE(TEST_BYTELEN(u8"\u00b1"));
+    REQUIRE(TEST_BYTELEN(u8"\u03b1"));
+    REQUIRE(TEST_BYTELEN(u8"\u03BA\u1F79\u03C3\u03BC\u03B5"));
+    REQUIRE(TEST_BYTELEN(u8"\u0288\u0119\u0835\u0E34\u021B"));
+  }
+  SECTION("UTF16")
+  {
+    REQUIRE(TEST_BYTELEN(u"\u000D"));
+    REQUIRE(TEST_BYTELEN(u"\u00b1"));
+    REQUIRE(TEST_BYTELEN(u"\u03b1"));
+    REQUIRE(TEST_BYTELEN(u"\u03BA\u1F79\u03C3\u03BC\u03B5"));
+    REQUIRE(TEST_BYTELEN(u"\u0288\u0119\u0835\u0E34\u021B"));
+  }
+  SECTION("UTF32")
+  {
+    REQUIRE(TEST_BYTELEN(U"\u000D"));
+    REQUIRE(TEST_BYTELEN(U"\u00b1"));
+    REQUIRE(TEST_BYTELEN(U"\u03b1"));
+    REQUIRE(TEST_BYTELEN(U"\u03BA\u1F79\u03C3\u03BC\u03B5"));
+    REQUIRE(TEST_BYTELEN(U"\u0288\u0119\u0835\u0E34\u021B"));
+  }
+
+#undef TEST_BYTELEN
+}
