38f0b4dff8fb40060422fd4ec37bc84ab2ca65ca
R533-Code <raphaeldibnehmepc@gmail.com>
2024-08-05 22:39:26 +0200

    Added test for some unicode functiosn

--- test/dsa/test_unicode.cpp
+++ test/dsa/test_unicode.cpp
@@ -66,3 +66,74 @@ TEST_CASE("Unicode Size", "[bytelen]")
 
 #undef TEST_BYTELEN
 }
+
+TEST_CASE("Unicode UTF32 to UTF8", "[utf32_to_utf8]")
+{
+  using namespace clt;
+  using namespace clt::uni;
+  using enum StringEncoding;
+
+  char32_t A[]          = U"\u0288\u0119\u0835\u0E34\u021B";
+  const size_t sizeofA  = sizeof A / sizeof(char32_t);
+  char8_t A8[]          = u8"\u0288\u0119\u0835\u0E34\u021B";
+  const size_t sizeofA8 = sizeof A8 / sizeof(char8_t);
+  char8_t buffer[256]   = {0};
+  auto result           = &buffer[0];
+
+  SECTION("UTF32 HOST VALID")
+  {
+    auto from = ptr_to<const Char32*>(&A[0]);
+    REQUIRE(to_utf8(from, 6, result, 256) == ConvError::NO_ERROR);
+    REQUIRE(
+        std::memcmp(
+            ptr_to<const void*>(A8), ptr_to<const void*>(buffer),
+            result - &buffer[0])
+        == 0);
+  }
+
+  SECTION("UTF32 HOST INVALID")
+  {
+    for (size_t i = 0; i < sizeofA; i++)
+      A[i] = bit::byteswap((u32)A[i]);
+    
+    // The reversed bytes will be invalid unicode
+    auto from = ptr_to<const Char32*>(&A[0]);
+    REQUIRE(to_utf8(from, 6, result, 256) == ConvError::INVALID_INPUT);
+  }
+
+  SECTION("UTF32 HOST NOT ENOUGH MEMORY")
+  {
+    auto from = ptr_to<const Char32*>(&A[0]);
+    REQUIRE(to_utf8(from, 6, result, 8) == ConvError::NOT_ENOUGH_SPACE);
+  }
+
+  SECTION("UTF32 BYTESWAP VALID")
+  {
+    for (size_t i = 0; i < sizeofA; i++)
+      A[i] = bit::byteswap((u32)A[i]);
+
+    auto from = ptr_to<const Char32Other*>(&A[0]);
+    REQUIRE(to_utf8(from, 6, result, 256) == ConvError::NO_ERROR);
+    REQUIRE(
+        std::memcmp(
+            ptr_to<const void*>(A8), ptr_to<const void*>(buffer),
+            result - &buffer[0])
+        == 0);
+  }
+
+  SECTION("UTF32 BYTESWAP INVALID")
+  {
+    // The reversed bytes will be invalid unicode
+    auto from = ptr_to<const Char32Other*>(&A[0]);
+    REQUIRE(to_utf8(from, 6, result, 256) == ConvError::INVALID_INPUT);
+  }
+
+  SECTION("UTF32 BYTESWAP NOT ENOUGH MEMORY")
+  {
+    for (size_t i = 0; i < sizeofA; i++)
+      A[i] = bit::byteswap((u32)A[i]);
+
+    auto from = ptr_to<const Char32Other*>(&A[0]);
+    REQUIRE(to_utf8(from, 6, result, 8) == ConvError::NOT_ENOUGH_SPACE);
+  }
+}
