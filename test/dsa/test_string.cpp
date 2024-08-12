1bc9366a2505bdab5b7555c20afffc8b9c8b7ca6
R533-Code <raphaeldibnehmepc@gmail.com>
2024-08-12 22:14:56 +0200

    Added test for string

---
+++ test/dsa/test_string.cpp
@@ -0,0 +1,20 @@
+#include <catch2/catch_all.hpp>
+#include <colt/dsa/string.h>
+
+TEST_CASE("String Indexing", "[String operator INDEX]")
+{
+  using namespace clt;
+
+  SECTION("UTF8")
+  {
+    u8StringView a = ptr_to<const Char8*>(u8"10\u03BC\u00BC");
+    u8String<mem::Mallocator> ab = {mem::Mallocator{}, a};
+    fmt::print("{}", ab);
+    REQUIRE(ab.front() == U'1');
+    REQUIRE(ab[0] == U'1');
+    REQUIRE(ab[1] == U'0');
+    REQUIRE(ab[2] == U'\u03BC');
+    REQUIRE(ab[3] == U'\u00BC');
+    REQUIRE(ab.back() == U'\u00BC');
+  }  
+}
