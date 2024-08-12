3a661544f342f4252da19a59809dcc405d69a01d
R533-Code <raphaeldibnehmepc@gmail.com>
2024-08-12 15:20:27 +0200

    Added partial implementation of BasicString

---
+++ include/colt/dsa/string.cpp
@@ -0,0 +1,31 @@
+#include "string.h"
+
+#if defined(COLT_LINUX)
+extern char etext, edata, end; // The symbols must have some type to avoid warnings
+#elif defined(COLT_WINDOWS)
+  #include <Windows.h>
+  #include <Psapi.h>
+#endif
+
+bool clt::maybe_in_const_segment(const void* ptr) noexcept
+{
+  // Credits Morgan McGuire, Zander Majercik
+  // https://www.youtube.com/watch?v=fglXeSWGVDc
+#if defined(COLT_LINUX)
+  return (ptr < &edata) && (ptr > &etext);
+#elif defined(COLT_WINDOWS)
+  // Initialization will be performed once (Meyer's Singleton)
+  static MODULEINFO info;
+  static const bool BOOL = GetModuleInformation(
+      GetCurrentProcess(), GetModuleHandleA(nullptr), &info, sizeof(MODULEINFO));
+  // To avoid unused warning
+  (void)BOOL;
+  return uintptr_t(info.EntryPoint) < uintptr_t(ptr)
+         && uintptr_t(ptr)
+                < uintptr_t(info.EntryPoint) + uintptr_t(info.SizeOfImage);
+#else
+  static const char* PROBE        = "___Colt_Internal_Str___";
+  static const auto PROBE_ADDRESS = uintptr_t(PROBE);
+  return math::abs(i64(uintptr_t(ptr) - PROBE_ADDRESS)) < 5'242'880;
+#endif
+}
---
+++ include/colt/dsa/string.h
@@ -0,0 +1,513 @@
+#ifndef HG_DSA_STRING
+#define HG_DSA_STRING
+
+#include "string_view.h"
+#include "colt/mem/composable_alloc.h"
+
+namespace clt
+{
+  namespace meta
+  {
+    /// @brief Used to customize BasicString
+    struct StringCustomization
+    {
+      /// @brief The encoding of the current string
+      StringEncoding ENCODING;
+      /// @brief Buffer size
+      /// @warning Use buffer_bytesize to access
+      size_t BUFFER_SIZE;
+      /// @brief If the buffer is big enough, cache the count of code point.
+      /// This might affect performance if the count is not being used, as
+      /// the string must count each characters that is appended or removed.
+      bool CACHE_COUNT;
+      /// @brief If the buffer is big enough, cache the middle code point.
+      /// This can be improve indexing speed by counting from the middle
+      /// of the string.
+      bool CACHE_MIDDLE;
+      /// @brief If the current platform supports it, the string will not make copy
+      /// of literals inside of the 'rdata' section unless a modification occurs.
+      bool NO_COPY_IF_CONST;
+
+      /// @brief Check if the string has a Small String Optimization buffer.
+      /// The size of that buffer is calculated by 'buffer_bytesize'.
+      /// @return
+      constexpr bool has_buffer() const noexcept { return BUFFER_SIZE != 0; }
+      /// @brief True if the string must cache the count of code point stored.
+      /// @return True if CACHE_COUNT
+      constexpr bool has_count() const noexcept
+      {
+        return is_variadic_encoding(ENCODING) && CACHE_COUNT;
+      }
+      /// @brief True if the string must cache the offset to the middle code point.
+      /// @return True if CACHE_MIDDLE
+      constexpr bool has_middle() const noexcept
+      {
+        return is_variadic_encoding(ENCODING) && CACHE_MIDDLE;
+      }
+      /// @brief True if the string will not make copy of literal unless a modification occurs.
+      /// If the current platform does not support it, then returns false.
+      /// @return True if the current platform supports it and NO_COPY_IF_CONST is true.
+      constexpr bool has_cow() const noexcept
+      {
+#if defined(COLT_WINDOWS) || defined(COLT_LINUX)
+        return NO_COPY_IF_CONST;
+#else
+        return false;
+#endif // COLT_WINDOWS
+      }
+
+      /// @brief Returns the SSO buffer size.
+      /// The buffer size is NOT equal to BUFFER_SIZE, but is rather aligned to
+      /// the next multiple of 8.
+      /// @return The SSO buffer size
+      constexpr size_t buffer_bytesize() const noexcept
+      {
+        // Align to 8 byte boundary.
+        return (BUFFER_SIZE + 7) & static_cast<size_t>(-8);
+      }
+    };
+  } // namespace meta
+
+  namespace details
+  {
+    struct SSOMiddle
+    {
+      size_t value{};
+    };
+    
+    struct SSOCount
+    {
+      size_t value{};
+    };
+
+    template<bool CACHE_MIDDLE>
+    using SSOCacheMiddle = std::conditional_t<CACHE_MIDDLE, SSOCount, meta::empty_t>;
+    template<bool CACHE_COUNT>
+    using SSOCacheCount = std::conditional_t<CACHE_COUNT, SSOMiddle, meta::empty_t>;
+
+    template<meta::CharType Ty, bool CACHE_MIDDLE, bool CACHE_COUNT>
+    class SSOLongInfo
+        : private SSOCacheCount<CACHE_COUNT>
+        , private SSOCacheMiddle<CACHE_MIDDLE>
+    {
+      Ty* _ptr;
+
+    public:
+      constexpr SSOLongInfo() noexcept = default;
+      MAKE_DEFAULT_COPY_AND_MOVE_FOR(SSOLongInfo);
+
+      constexpr auto& ptr() noexcept { return _ptr; }
+      constexpr auto& ptr() const noexcept { return _ptr; }
+
+      constexpr auto& count() noexcept
+        requires CACHE_COUNT
+      {
+        return SSOCacheCount<CACHE_COUNT>::value;
+      }
+      constexpr auto& count() const noexcept
+        requires CACHE_COUNT
+      {
+        return SSOCacheCount<CACHE_COUNT>::value;
+      }
+      constexpr auto& middle() noexcept
+        requires CACHE_MIDDLE
+      {
+        return SSOCacheMiddle<CACHE_MIDDLE>::value;
+      }
+      constexpr auto& middle() const noexcept
+        requires CACHE_COUNT
+      {
+        return SSOCacheMiddle<CACHE_MIDDLE>::value;
+      }
+    };
+
+    template<meta::StringCustomization CUSTOMIZATION>
+    class SSOBuffer
+    {
+    public:
+      using char_t = meta::encoding_to_char_t<CUSTOMIZATION.ENCODING>;
+
+    private:
+      union
+      {
+        SSOLongInfo<
+            meta::encoding_to_char_t<CUSTOMIZATION.ENCODING>,
+            CUSTOMIZATION.CACHE_MIDDLE, CUSTOMIZATION.CACHE_COUNT>
+            _long;
+
+        char_t _buffer[CUSTOMIZATION.buffer_bytesize() / sizeof(char_t)];
+      };
+
+    public:
+      constexpr auto& ptr() noexcept { return _long.ptr(); }
+      constexpr auto& ptr() const noexcept { return _long.ptr(); }
+
+      constexpr auto& buffer() noexcept
+        requires(CUSTOMIZATION.has_buffer())
+      {
+        return _buffer;
+      }
+      constexpr auto& buffer() const noexcept
+        requires(CUSTOMIZATION.has_buffer())
+      {
+        return _buffer;
+      }
+      constexpr auto& count() noexcept
+        requires(CUSTOMIZATION.has_count())
+      {
+        return _long.count();
+      }
+      constexpr auto& count() const noexcept
+        requires(CUSTOMIZATION.has_count())
+      {
+        return _long.count();
+      }
+      constexpr auto& middle() noexcept
+        requires(CUSTOMIZATION.has_middle())
+      {
+        return _long.middle();
+      }
+      constexpr auto& middle() const noexcept
+        requires(CUSTOMIZATION.has_middle())
+      {
+        return _long.middle();
+      }
+
+      SSOBuffer() noexcept {}
+      MAKE_DEFAULT_COPY_AND_MOVE_FOR(SSOBuffer);
+    };
+  } // namespace details
+
+  template<meta::StringCustomization CUSTOMIZATION, typename ALLOCATOR>
+  class BasicString : private ALLOCATOR
+  {
+  public:
+    using char_t = meta::encoding_to_char_t<CUSTOMIZATION.ENCODING>;
+    /// @brief The string encoding
+    static constexpr StringEncoding STR_ENCODING = CUSTOMIZATION.ENCODING;
+    /// @brief True if the string has a SSO buffer
+    static constexpr bool HAS_BUFFER = CUSTOMIZATION.has_buffer();
+    /// @brief True if the string caches the code point count
+    static constexpr bool HAS_COUNT = CUSTOMIZATION.has_count();
+    /// @brief True if the string caches the middle code point
+    static constexpr bool HAS_MIDDLE = CUSTOMIZATION.has_middle();
+    /// @brief True if the string supports Copy On Write for literals
+    static constexpr bool HAS_COW = CUSTOMIZATION.has_cow();
+    /// @brief The size of the SSO buffer
+    static constexpr size_t SSO_SIZE =
+        CUSTOMIZATION.buffer_bytesize() / sizeof(char_t);
+
+  private:
+    details::SSOBuffer<CUSTOMIZATION> _ptr_or_buffer;
+    size_t _size;
+    size_t _capacity;
+
+    bool _is_long() const noexcept
+      requires HAS_BUFFER
+    {
+      return _capacity != SSO_SIZE;
+    }
+
+    /// @brief The true size of the string (including NUL terminator)
+    /// @return _size + 1 to account for terminator
+    size_t _true_size() const noexcept { return _size + 1; }
+
+    /// @brief Allocates more memory for the string.
+    /// This modifies the capacity (and data()).
+    /// @param new_capacity The new capacity
+    void alloc(size_t new_capacity) noexcept
+    {
+      if constexpr (HAS_BUFFER)
+      {
+        // No allocations needed
+        if (new_capacity < SSO_SIZE)
+        {
+          _capacity = SSO_SIZE;
+          return;
+        }
+      }
+      auto blk = ALLOCATOR::alloc((new_capacity + _capacity) * sizeof(char_t));
+      ::memcpy(data(), blk.ptr(), _true_size());
+      dealloc();
+      _ptr_or_buffer.ptr() = ptr_to<char_t*>(blk.ptr());
+      _capacity = new_capacity;
+    }
+
+    /// @brief Deallocates current allocated memory.
+    /// This does not modify any of the members.
+    void dealloc() noexcept
+    {
+      if constexpr (HAS_BUFFER)
+      {
+        if (_is_long())
+          ALLOCATOR::dealloc({_ptr_or_buffer.ptr(), _capacity * sizeof(char_t)});
+        // No need to deallocate as internal buffer is used instead
+      }
+      else
+        ALLOCATOR::dealloc(
+            {_ptr_or_buffer.ptr() * (_capacity != 0), _capacity * sizeof(char_t)});
+      // The multiplication by (_capacity != 0) is used to eliminate
+      // the case where the BasicString is initialized to "".
+    }
+
+  public:
+    /// @brief Constructs an empty string
+    BasicString(const ALLOCATOR& ALLOC) noexcept
+      requires HAS_BUFFER
+        : ALLOCATOR(ALLOC)
+        , _size(1)
+        , _capacity(SSO_SIZE)
+    {
+      _ptr_or_buffer.buffer()[0] = '\0';
+    }
+
+    BasicString(const ALLOCATOR& ALLOC) noexcept
+      requires(!HAS_BUFFER)
+        : ALLOCATOR(ALLOC)
+        , _size(0)
+        , _capacity(0)
+    {
+      _ptr_or_buffer.ptr() = "";
+    }
+
+    BasicString(
+        const ALLOCATOR& ALLOC, char_t FILL, size_t count,
+        size_t added_capacity = 0) noexcept
+        : ALLOCATOR(ALLOC)
+        , _size(count)
+        , _capacity(0)
+    {
+      alloc(count + added_capacity + 1);
+      std::fill_n(data(), count, FILL);
+      data()[_size] = '\0';
+    }
+
+    template<bool IS_ZSTRING>
+    BasicString(
+        const ALLOCATOR& ALLOC,
+        BasicStringView<STR_ENCODING, IS_ZSTRING> str,
+        size_t added_capacity = 0) noexcept
+        : ALLOCATOR(ALLOC)
+        , _size(str.byte_size() / sizeof(char_t))
+        , _capacity(0)
+    {
+      alloc(_size + added_capacity + 1);
+      ::memcpy(data(), str.data(), sizeof(char_t) * _size);
+      data()[_size] = '\0';
+    }
+
+    ~BasicString() { dealloc(); }
+
+    /// @brief Returns a pointer to the data owned by the string
+    /// @return Pointer to the data owned by the string
+    const char_t* data() const noexcept
+    {
+      if constexpr (HAS_BUFFER)
+        return _is_long() ? _ptr_or_buffer.ptr() : _ptr_or_buffer.buffer();
+      else
+        return _ptr_or_buffer.ptr();
+    }
+
+    /// @brief Returns a pointer to the data owned by the string
+    /// @return Pointer to the data owned by the string
+    char_t* data() noexcept
+    {
+      // Thanks Scott!
+      return const_cast<char_t*>(static_cast<const BasicString&>(*this).data());
+    }
+
+    /// @brief Returns the capacity of the string
+    /// @return The capacity of the string
+    size_t capacity() const noexcept { return _capacity; }
+
+    /// @brief Returns the count of code points in the string
+    /// This does not include the NUL terminator.
+    /// @return strlen of the string
+    size_t size() const noexcept
+    {
+      if constexpr (HAS_COUNT)
+        return _is_long() ? _ptr_or_buffer.count()
+                          : clt::strlen(_ptr_or_buffer.buffer());
+      else
+        return clt::strlen(data());
+    }
+
+    /// @brief Returns the byte size over which the string is spanning.
+    /// This does not include the NUL terminator.
+    /// @return The byte size
+    size_t byte_size() const noexcept { return _size * sizeof(char_t); }
+
+    /// @brief Check if the current string is empty.
+    /// Empty strings are still NUL terminated.
+    /// @return True if size() == 0
+    bool is_empty() const noexcept { return byte_size() == 0; }
+
+    /// @brief Returns the char at index 'index'.
+    /// Do not use this operator to iterate over the view:
+    /// use a ranged for loop for performance.
+    /// Same as 'index_front'.
+    /// @pre index < size()
+    /// @param index The index of the char to return
+    /// @return The char at index 'index'
+    constexpr char32_t operator[](size_t index) const noexcept
+    {
+      return index_front(index);
+    }
+
+    /// @brief Returns the char at index 'index'.
+    /// Do not use this operator to iterate over the view:
+    /// use a ranged for loop for performance.
+    /// Same as 'index_front'.
+    /// @pre index < size()
+    /// @param index The index of the char to return
+    /// @return The char at index 'index'
+    constexpr char32_t index_front(size_t index) const noexcept
+    {
+      assert_true("Invalid index!", index < size());
+      // If the size is not cached, then computing the size
+      // will take more time than just indexing.
+      if constexpr (HAS_MIDDLE && HAS_COUNT)
+      {
+        if (_is_long())
+        {
+          const size_t cached_size         = size();
+          const size_t middle_cached_size  = cached_size / 2;
+          const size_t quarter_cached_size = cached_size / 4;
+          if (index >= middle_cached_size)
+          {
+            return uni::index_front(
+                _ptr_or_buffer.ptr() + _ptr_or_buffer.middle(),
+                index - middle_cached_size);
+          }
+          if (index > quarter_cached_size)
+          {
+            return uni::index_back(
+                _ptr_or_buffer.ptr() + _ptr_or_buffer.middle(),
+                middle_cached_size - index);
+          }
+          return uni::index_front(_ptr_or_buffer.ptr(), index);
+        }
+        return uni::index_front(_ptr_or_buffer.buffer(), index);
+      }
+      else
+        return uni::index_front(data(), index);
+    }
+
+    constexpr char32_t index_back(size_t index) const noexcept
+    {
+      assert_true("Invalid index!", index < size());
+      // If the size is not cached, then computing the size
+      // will take more time than just indexing.
+      if constexpr (HAS_MIDDLE && HAS_COUNT)
+      {
+        if (_is_long())
+        {
+          const size_t cached_size         = size();
+          const size_t middle_cached_size  = cached_size / 2;
+          const size_t quarter_cached_size = 3 * (cached_size / 4);
+          if (index < middle_cached_size)
+          {
+            return uni::index_back(
+                _ptr_or_buffer.ptr() + _ptr_or_buffer.middle(),
+                middle_cached_size - index);
+          }
+          if (index <= quarter_cached_size)
+          {
+            return uni::index_front(
+                _ptr_or_buffer.ptr() + _ptr_or_buffer.middle(),
+                index - middle_cached_size);
+          }
+          return uni::index_back(_ptr_or_buffer.ptr() + _size - 1, index);
+        }
+        return uni::index_back(_ptr_or_buffer.buffer() + _size - 1, index);
+      }
+      else
+        return uni::index_back(data() + _size - 1, index);
+    }
+
+    /// @brief Get the front of the view.
+    /// @return The first item of the view
+    char32_t front() const noexcept
+    {
+      assert_true("Invalid size!", !is_empty());
+      return index_front(0);
+    }
+
+    /// @brief Get the back of the view.
+    /// @return The last item of the view
+    char32_t back() const noexcept
+    {
+      assert_true("Invalid size!", !is_empty());
+      return index_back(0);
+    }
+
+    /// @brief Returns a NUL-terminated string.
+    /// This is always guaranteed to work without allocation.
+    /// @return NUL-terminated string
+    auto c_str() const noexcept { return data(); }
+    /// @brief Returns a NUL-terminated string.
+    /// This is always guaranteed to work without allocation.
+    /// @return NUL-terminated string
+    auto c_str() noexcept { return data(); }
+
+    /// @brief Returns a StringView over the characters
+    /// @return StringView over all the characters
+    StringView<STR_ENCODING> to_view() const noexcept { return {data(), _size}; }
+    /// @brief Returns a StringView over the characters
+    operator StringView<STR_ENCODING>() const noexcept { return {data(), _size}; }
+
+    /// @brief Returns a ZStringView over the characters
+    /// @return StringView over all the characters
+    ZStringView<STR_ENCODING> to_zview() const noexcept { return {data(), _size}; }
+    /// @brief Returns a ZStringView over the characters
+    operator ZStringView<STR_ENCODING>() const noexcept { return {data(), _size}; }
+
+    /// @brief Lexicographically compare two views
+    /// @param v1 The first view
+    /// @param v2 The second view
+    /// @return Result of comparison
+    template<StringEncoding ENCODING2, bool ZSTRING2>
+    friend constexpr auto operator<=>(
+        const BasicString& v1,
+        const BasicStringView<ENCODING2, ZSTRING2>& v2) noexcept
+    {
+      return std::lexicographical_compare_three_way(
+          v1.begin(), v1.end(), v2.begin(), v2.end());
+    }
+  };
+
+  /// @brief Check if a pointer (maybe) points into the const segment
+  /// of the executable.
+  /// @param ptr The pointer to check for
+  /// @return True if in const segment
+  bool maybe_in_const_segment(const void* ptr) noexcept;
+
+  template<typename ALLOCATOR, StringEncoding ENCODING = StringEncoding::ASCII>
+  using String = BasicString<meta::StringCustomization{ ENCODING, 24, true, true, true }, ALLOCATOR>;
+  template<typename ALLOCATOR>
+  using u8String = String<ALLOCATOR, StringEncoding::UTF8>;
+  template<typename ALLOCATOR>
+  using u16String = String<ALLOCATOR, StringEncoding::UTF16>;
+  template<typename ALLOCATOR>
+  using u32String = String<ALLOCATOR, StringEncoding::UTF32>;
+} // namespace clt
+
+template<clt::meta::StringCustomization CUSTOMIZATION, typename ALLOCATOR>
+struct fmt::formatter<clt::BasicString<CUSTOMIZATION, ALLOCATOR>>
+{
+  using to_format = clt::BasicString<CUSTOMIZATION, ALLOCATOR>;
+
+  template<typename ParseContext>
+  constexpr auto parse(ParseContext& ctx)
+  {
+    return ctx.begin();
+  }
+
+  template<typename FormatContext>
+  auto format(const to_format& vec, FormatContext& ctx) const
+  {
+    return format_to(ctx.out(), "{}", vec.to_view());
+  }
+};
+
+#endif // !HG_DSA_STRING
