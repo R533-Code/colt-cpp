7f6c402b5e2e05d345d03ba8a46c70d5e637bdb1
R533-Code <raphaeldibnehmepc@gmail.com>
2024-08-15 01:29:56 +0200

    Added custom main

--- test/test_run.cpp
+++ test/test_run.cpp
@@ -1,10 +1,23 @@
-#define CONFIG_CATCH_MAIN
-#include <catch2/catch_all.hpp>
+#include <catch2/catch_session.hpp>
+#include <colt/macro/config.h>
+#include <colt/bit/detect_simd.h>
+#include <colt/io/print.h>
 
-// TEST_CASE("Factorials are computed", "[factorial]")
-// {
-//   REQUIRE(test_factorial(1) == 1);
-//   REQUIRE(test_factorial(2) == 2);
-//   REQUIRE(test_factorial(3) == 6);
-//   REQUIRE(test_factorial(10) == 3'628'800);
-// }
+int main(int argc, char* argv[])
+{
+  using namespace clt;
+  using namespace clt::io;  
+  
+  auto result = Catch::Session().run(argc, argv);
+  fmt::println(stderr,
+      "Running tests on {}{} {}{}{} ({}).\nThis is a {}-endian system.\nSupported "
+      "SIMD instructions: {}{}{}.\n",
+      CyanF, COLT_OS_STRING, MagentaF, COLT_ARCH_STRING, Reset, COLT_CONFIG_STRING,
+#ifdef COLT_LITTLE_ENDIAN
+      "little",
+#else
+      "big",
+#endif // COLT_LITTLE_ENDIAN
+      BrightBlueF, bit::detect_supported_architectures(), Reset);
+  return result;
+}
