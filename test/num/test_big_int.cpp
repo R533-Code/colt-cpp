3bd8e6931e66eabb70e280227ce87b1283471e8e
R533-Code <raphaeldibnehmepc@gmail.com>
2024-08-16 22:23:49 +0200

    Added empty test

--- test/num/test_big_int.cpp
+++ test/num/test_big_int.cpp
@@ -1,7 +1,19 @@
 #include <catch2/catch_all.hpp>
 #include <colt/num/big_int.h>
+#include <colt/num/big_rational.h>
 
 TEST_CASE("BigInt", "[big_int]")
 {
+  using namespace clt;
+  using namespace clt::num;
+
+  auto a = BigInt{1000000};
+}
+
+TEST_CASE("BigRational", "[big_rational]")
+{
+  using namespace clt;
+  using namespace clt::num;
   
+  auto b = BigRational{1000000};
 }
