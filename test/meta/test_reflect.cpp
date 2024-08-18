76d36ccd0f11fee9c0c85a0e7921261e90aeb24c
R533-Code <raphaeldibnehmepc@gmail.com>
2024-08-18 10:59:37 +0200

    Added more test cases

--- test/meta/test_reflect.cpp
+++ test/meta/test_reflect.cpp
@@ -6,7 +6,11 @@ TEST_CASE("reflect", "[reflect::str]")
   using namespace clt;
   using namespace clt::meta;  
   REQUIRE(reflect<i32>::str() == "i32");
+  REQUIRE(reflect<i32*>::str() == "i32*");
   REQUIRE(reflect<const i32>::str() == "const i32");
+  REQUIRE(reflect<const i32*>::str() == "const i32*");
   REQUIRE(reflect<volatile i32>::str() == "volatile i32");
+  REQUIRE(reflect<volatile i32*>::str() == "volatile i32*");
   REQUIRE(reflect<const volatile i32>::str() == "const volatile i32");
+  REQUIRE(reflect<const volatile i32*>::str() == "const volatile i32*");
 }
