7c6412dd382dbd7096148de138e992633ec956bc
R533-Code <raphaeldibnehmepc@gmail.com>
2024-08-20 19:23:35 +0200

    Added AVX2 and AVX512 version of strlen16

--- include/colt/dsa/unicode.cpp
+++ include/colt/dsa/unicode.cpp
@@ -74,7 +74,7 @@ static size_t unitlen32default(const char32_t* ptr) noexcept
 
 #if defined(COLT_x86_64)
 
-#pragma region // strlen8 SSE2, AVX2, AXV512BW
+  #pragma region // strlen8 SSE2, AVX2, AXV512BW
 static COLT_FORCE_SSE2 size_t strlen8SSE2(const char8_t* ptr) noexcept
 {
   size_t len = 0;
@@ -203,9 +203,9 @@ static COLT_FORCE_AVX512BW size_t strlen8AXV512BW(const char8_t* ptr) noexcept
   clt::unreachable("programming error");
 }
 
-#pragma endregion
+  #pragma endregion
 
-#pragma region // strlen16 SSE2, AVX2, AVX512BW
+  #pragma region // strlen16 SSE2, AVX2, AVX512BW
 
 template<bool SWAP>
 static COLT_FORCE_SSE2 size_t strlen16SSE2(const char16_t* ptr) noexcept
@@ -249,7 +249,7 @@ static COLT_FORCE_SSE2 size_t strlen16SSE2(const char16_t* ptr) noexcept
   // In any cases, there are at most 16 byte to check.
   while (true)
   {
-    if (*ptr == '\0')
+    if (*ptr == 0)
       return len;
     if constexpr (SWAP)
       len += (size_t)(!clt::uni::is_trail_surrogate(clt::bit::byteswap(*ptr)));
@@ -260,9 +260,111 @@ static COLT_FORCE_SSE2 size_t strlen16SSE2(const char16_t* ptr) noexcept
   clt::unreachable("programming error");
 }
 
-#pragma endregion
+template<bool SWAP>
+static COLT_FORCE_AVX2 size_t strlen16AVX2(const char16_t* ptr) noexcept
+{
+  size_t len = 0;
+  // We can't use sequence_length here as the goal is to align
+  // and most likely adding sequence_length will not align the pointer.
+  while (uintptr_t(ptr) % 32 != 0)
+  {
+    if (*ptr == 0)
+      return len;
+    if constexpr (SWAP)
+      len += (size_t)(!clt::uni::is_trail_surrogate(clt::bit::byteswap(*ptr)));
+    else
+      len += (size_t)(!clt::uni::is_trail_surrogate(*ptr));
+    ++ptr;
+  }
 
-#pragma region // unitlen16 SSE2, AVX2, AVX512F
+  const __m256i zero        = _mm256_setzero_si256();
+  const __m256i trail_mask  = _mm256_set1_epi16(SWAP ? (u16)0x00FC : (u16)0xFC00);
+  const __m256i trail_value = _mm256_set1_epi16(SWAP ? (u16)0x00DC : (u16)0xDC00);
+  constexpr auto PACK_COUNT = sizeof(__m256i) / sizeof(u16);
+  unsigned int mask;
+  while (true)
+  {
+    __m256i values = _mm256_load_si256(reinterpret_cast<const __m256i*>(ptr));
+    __m256i cmp    = _mm256_cmpeq_epi16(values, zero);
+    mask           = _mm256_movemask_epi8(cmp);
+    // If we found NUL-terminator break.
+    if (mask != 0)
+      break;
+    __m256i is_trail = _mm256_and_si256(values, trail_mask);
+    is_trail         = _mm256_cmpeq_epi16(is_trail, trail_value);
+    len +=
+        PACK_COUNT - std::popcount((unsigned int)_mm256_movemask_epi8(is_trail)) / 2;
+    ptr += PACK_COUNT;
+  }
+  // We might have ended on trailing byte
+  // so we can't really use sequence_length here.
+  // In any cases, there are at most 32 byte to check.
+  while (true)
+  {
+    if (*ptr == 0)
+      return len;
+    if constexpr (SWAP)
+      len += (size_t)(!clt::uni::is_trail_surrogate(clt::bit::byteswap(*ptr)));
+    else
+      len += (size_t)(!clt::uni::is_trail_surrogate(*ptr));
+    ++ptr;
+  }
+  clt::unreachable("programming error");
+}
+
+template<bool SWAP>
+static COLT_FORCE_AVX512BW size_t strlen16AVX512BW(const char16_t* ptr) noexcept
+{
+  size_t len = 0;
+  // We can't use sequence_length here as the goal is to align
+  // and most likely adding sequence_length will not align the pointer.
+  while (uintptr_t(ptr) % 32 != 0)
+  {
+    if (*ptr == 0)
+      return len;
+    if constexpr (SWAP)
+      len += (size_t)(!clt::uni::is_trail_surrogate(clt::bit::byteswap(*ptr)));
+    else
+      len += (size_t)(!clt::uni::is_trail_surrogate(*ptr));
+    ++ptr;
+  }
+
+  const __m512i zero        = _mm512_setzero_si512();
+  const __m512i trail_mask  = _mm512_set1_epi16(SWAP ? (u16)0x00FC : (u16)0xFC00);
+  const __m512i trail_value = _mm512_set1_epi16(SWAP ? (u16)0x00DC : (u16)0xDC00);
+  constexpr auto PACK_COUNT = sizeof(__m512i) / sizeof(u16);
+  unsigned int mask;
+  while (true)
+  {
+    __m512i values = _mm512_load_si512(reinterpret_cast<const __m512i*>(ptr));
+    mask           = _mm512_cmpeq_epi16_mask(values, zero);
+    // If we found NUL-terminator break.
+    if (mask != 0)
+      break;
+    __m512i is_trail = _mm512_and_si512(values, trail_mask);
+    len += PACK_COUNT
+           - std::popcount(_mm512_cmpeq_epi16_mask(is_trail, trail_value));
+    ptr += PACK_COUNT;
+  }
+  // We might have ended on trailing byte
+  // so we can't really use sequence_length here.
+  // In any cases, there are at most 32 byte to check.
+  while (true)
+  {
+    if (*ptr == 0)
+      return len;
+    if constexpr (SWAP)
+      len += (size_t)(!clt::uni::is_trail_surrogate(clt::bit::byteswap(*ptr)));
+    else
+      len += (size_t)(!clt::uni::is_trail_surrogate(*ptr));
+    ++ptr;
+  }
+  clt::unreachable("programming error");
+}
+
+  #pragma endregion
+
+  #pragma region // unitlen16 SSE2, AVX2, AVX512F
 
 static COLT_FORCE_SSE2 size_t unitlen16SSE2(const char16_t* ptr) noexcept
 {
@@ -345,9 +447,9 @@ static COLT_FORCE_AVX512F size_t unitlen16AVX512F(const char16_t* ptr) noexcept
   return (ptr - copy) + std::countr_zero(mask);
 }
 
-#pragma endregion
+  #pragma endregion
 
-#pragma region // unitlen32 SSE2, AVX2, AVX512F
+  #pragma region // unitlen32 SSE2, AVX2, AVX512F
 static COLT_FORCE_SSE2 size_t unitlen32SSE2(const char32_t* ptr) noexcept
 {
   const auto copy = ptr;
@@ -427,11 +529,11 @@ static COLT_FORCE_AVX512F size_t unitlen32AVX512F(const char32_t* ptr) noexcept
   }
   return (ptr - copy) + std::countr_zero(mask);
 }
-#pragma endregion
+  #pragma endregion
 
 #elif defined(COLT_ARM_7or8)
 
-#pragma region // unitlen16 NEON
+  #pragma region // unitlen16 NEON
 static COLT_FORCE_NEON size_t unitlen16NEON(const char16_t* ptr) noexcept
 {
   const auto copy = ptr;
@@ -454,9 +556,9 @@ static COLT_FORCE_NEON size_t unitlen16NEON(const char16_t* ptr) noexcept
   }
   return (ptr - copy) + std::countr_zero(mask) / 2;
 }
-#pragma endregion
+  #pragma endregion
 
-#pragma region // unitlen32 NEON
+  #pragma region // unitlen32 NEON
 static COLT_FORCE_NEON size_t unitlen32NEON(const char32_t* ptr) noexcept
 {
   const auto copy = ptr;
@@ -479,7 +581,7 @@ static COLT_FORCE_NEON size_t unitlen32NEON(const char32_t* ptr) noexcept
   }
   return (ptr - copy) + std::countr_zero(mask) / 2;
 }
-#pragma endregion
+  #pragma endregion
 
 #endif // COLT_x86_64
 
@@ -500,8 +602,9 @@ size_t clt::uni::details::strlen16LE(const char16_t* ptr) noexcept
 {
   using namespace clt::bit;
 #ifdef COLT_x86_64
-  static const auto FN = choose_simd_function<simd_flag::DEFAULT>(
-      &strlen16SSE2<(TargetEndian::native == TargetEndian::big)>);
+  static constexpr bool SWAP = (TargetEndian::native == TargetEndian::big);
+  static const auto FN = choose_simd_function<simd_flag::AVX512BW, simd_flag::AVX2, simd_flag::DEFAULT>(
+          &strlen16AVX512BW<SWAP>, &strlen16AVX2<SWAP>, &strlen16SSE2<SWAP>);
   return (*FN)(ptr);
 #else
   return strlen16LEdefault(ptr);
@@ -512,8 +615,10 @@ size_t clt::uni::details::strlen16BE(const char16_t* ptr) noexcept
 {
   using namespace clt::bit;
 #ifdef COLT_x86_64
-  static const auto FN = choose_simd_function<simd_flag::DEFAULT>(
-      &strlen16SSE2<(TargetEndian::native == TargetEndian::little)>);
+  static constexpr bool SWAP = (TargetEndian::native == TargetEndian::little);
+  static const auto FN =
+      choose_simd_function<simd_flag::AVX512BW, simd_flag::AVX2, simd_flag::DEFAULT>(
+          &strlen16AVX512BW<SWAP>, &strlen16AVX2<SWAP>, &strlen16SSE2<SWAP>);
   return (*FN)(ptr);
 #else
   return strlen16BEdefault(ptr);
