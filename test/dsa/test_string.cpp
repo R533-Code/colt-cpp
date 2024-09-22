9645e39ad4212841c65dfe2d33fc5e835532e0de
R533-Code <raphaeldibnehmepc@gmail.com>
2024-09-22 17:16:07 +0200

    Added more tests

--- test/dsa/test_string.cpp
+++ test/dsa/test_string.cpp
@@ -24,13 +24,13 @@ using AllStrings = std::tuple<
         clt::meta::StringCustomization{StringEncoding::UTF8, 0, true, true, true},
         mem::Mallocator>>;
 
-TEMPLATE_LIST_TEST_CASE("String Indexing", "[BasicString]", AllStrings)
+TEMPLATE_LIST_TEST_CASE("BasicString", "[BasicString]", AllStrings)
 {
   using namespace clt;
 
   using StringType = TestType;
 
-  SECTION("UTF8")
+  SECTION("Index long")
   {
     u8StringView a =
         ptr_to<const Char8*>(u8"10\u03BC\u00BC10\u03BC\u00BC10\u03BC\u00BC10\u03BC"
@@ -42,5 +42,46 @@ TEMPLATE_LIST_TEST_CASE("String Indexing", "[BasicString]", AllStrings)
     REQUIRE(ab[2] == U'\u03BC');
     REQUIRE(ab[3] == U'\u00BC');
     REQUIRE(ab.back() == U'\u00BC');
+  }
+  SECTION("Index short")
+  {
+    u8StringView a =
+        ptr_to<const Char8*>(u8"10\u03BC\u00BC");
+    StringType ab = {mem::Mallocator{}, a};
+    REQUIRE(ab.front() == U'1');
+    REQUIRE(ab[0] == U'1');
+    REQUIRE(ab[1] == U'0');
+    REQUIRE(ab[2] == U'\u03BC');
+    REQUIRE(ab[3] == U'\u00BC');
+    REQUIRE(ab.back() == U'\u00BC');
+  }
+  SECTION("Add long")
+  {
+    u8StringView a =
+        ptr_to<const Char8*>(u8"10\u03BC\u00BC10\u03BC\u00BC10\u03BC\u00BC10\u03BC"
+                             u8"\u00BC10\u03BC\u00BC10\u03BC\u00BC10\u03BC\u00BC");
+    StringType ab = {mem::Mallocator{}, a};
+    ab += u8StringView{ptr_to<const Char8*>(u8"Hello World!")};
+    REQUIRE(ab.front() == U'1');
+    REQUIRE(ab[0] == U'1');
+    REQUIRE(ab[1] == U'0');
+    REQUIRE(ab[2] == U'\u03BC');
+    REQUIRE(ab[3] == U'\u00BC');
+    REQUIRE(ab.back() == U'!');
+    REQUIRE(ab.index_back(1) == U'd');
+  }
+  SECTION("Add long")
+  {
+    u8StringView a =
+        ptr_to<const Char8*>(u8"10\u03BC\u00BC");
+    StringType ab = {mem::Mallocator{}, a};
+    ab += u8StringView{ptr_to<const Char8*>(u8"d!")};
+    REQUIRE(ab.front() == U'1');
+    REQUIRE(ab[0] == U'1');
+    REQUIRE(ab[1] == U'0');
+    REQUIRE(ab[2] == U'\u03BC');
+    REQUIRE(ab[3] == U'\u00BC');
+    REQUIRE(ab.back() == U'!');
+    REQUIRE(ab.index_back(1) == U'd');
   }
 }
