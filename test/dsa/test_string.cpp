4b35c5f854381f1ee8947216dacc4736c73513c4
R533-Code <raphaeldibnehmepc@gmail.com>
2024-09-22 14:13:17 +0200

    Improved tests and moved test_unicode to uni

--- test/dsa/test_string.cpp
+++ test/dsa/test_string.cpp
@@ -14,7 +14,7 @@ TEST_CASE("String Indexing")
 
   SECTION("UTF8")
   {
-    u8StringView a               = ptr_to<const Char8*>(u8"10\u03BC\u00BC");
+    u8StringView a               = ptr_to<const Char8*>(u8"10\u03BC\u00BC10\u03BC\u00BC10\u03BC\u00BC10\u03BC\u00BC10\u03BC\u00BC10\u03BC\u00BC10\u03BC\u00BC");
     u8String<mem::Mallocator> ab = {mem::Mallocator{}, a};
     REQUIRE(ab.front() == U'1');
     REQUIRE(ab[0] == U'1');
