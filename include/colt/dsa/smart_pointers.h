21e3aa767f907c57a94071254c97a769aaefe1b4
R533-Code <raphaeldibnehmepc@gmail.com>
2024-09-29 14:38:38 +0200

    Fixed allocator ref and added UniquePtr

---
+++ include/colt/dsa/smart_pointers.h
@@ -0,0 +1,208 @@
+#ifndef HG_COLT_SMART_POINTERS
+#define HG_COLT_SMART_POINTERS
+
+#include <colt/macro/on_scope_exit.h>
+#include <colt/mem/allocator_ref.h>
+
+namespace clt
+{
+  template<typename T, meta::Allocator ALLOCATOR>
+  /// @brief Unique pointer that automatically frees an allocated resource.
+  /// A unique pointer is movable but not copyable, which makes it own its resource.
+  class UniquePtr : private ALLOCATOR
+  {
+    /// @brief The memory block owned
+    mem::MemBlock blk = {};
+
+  public:
+    template<typename, meta::Allocator>
+    friend class UniquePtr;
+
+    UniquePtr()                            = delete;
+    UniquePtr(const UniquePtr&)            = delete;
+    UniquePtr& operator=(const UniquePtr&) = delete;
+
+    /// @brief Constructs an empty UniquePtr
+    /// @param alloc The allocator
+    constexpr UniquePtr(const ALLOCATOR& alloc) noexcept
+        : ALLOCATOR(alloc)
+    {
+    }
+
+    /// @brief Constructs a UniquePtr from a MemBlock.
+    /// @param alloc The allocator
+    /// @param blk The block to use
+    constexpr UniquePtr(const ALLOCATOR& alloc, mem::MemBlock blk) noexcept
+        : ALLOCATOR(alloc)
+        , blk(blk)
+    {
+    }
+
+    /// @brief Move constructor, steals the resources of 'u'
+    /// @param u The unique pointer whose resources to steal
+    constexpr UniquePtr(UniquePtr<T, ALLOCATOR>&& u) noexcept
+        : ALLOCATOR(u)
+        , blk(std::exchange(u.blk, mem::nullblk))
+    {
+    }
+
+    template<typename U>
+      requires std::convertible_to<U*, T*>
+    /// @brief Converting move constructor, steals the resources of 'u'
+    /// @tparam U Type that is convertible to T
+    /// @param u The unique pointer whose resources to steal
+    constexpr UniquePtr(UniquePtr<U, ALLOCATOR>&& u) noexcept
+        : ALLOCATOR(u)
+        , blk(std::exchange(u.blk, mem::nullblk))
+    {
+    }
+
+    /// /// @brief Destructor, frees resource
+    constexpr ~UniquePtr() noexcept(std::is_nothrow_destructible_v<T>)
+    {
+      if (blk.is_null())
+        return;
+      ON_SCOPE_EXIT
+      {
+        ALLOCATOR::dealloc(blk);
+      };
+      //run destructor
+      static_cast<T*>(blk.ptr())->~T();
+    }
+
+    /// @brief Move assignment operator
+    /// @param r The pointer whose content to swap with
+    /// @return Self
+    constexpr UniquePtr& operator=(UniquePtr&& r) noexcept
+    {
+      std::swap(static_cast<ALLOCATOR&>(r), static_cast<ALLOCATOR&>(*this));
+      std::swap(blk, r.blk);
+      return *this;
+    }
+
+    template<typename U>
+      requires std::convertible_to<U*, T*>
+    /// @brief Move assignment operator
+    /// @param r The pointer whose content to swap with
+    /// @return Self
+    constexpr UniquePtr& operator=(UniquePtr<U, ALLOCATOR>&& u) noexcept
+    {
+      std::swap(static_cast<ALLOCATOR&>(u), static_cast<ALLOCATOR&>(*this));
+      std::swap(blk, u.blk);
+      return *this;
+    }
+
+    /// @brief Releases the owned block
+    /// @return The owned block
+    constexpr mem::MemBlock release() noexcept
+    {
+      return std::exchange(blk, mem::nullblk);
+    }
+
+    /// @brief Frees resources and take ownership of 'ptr'
+    /// @param ptr The block whose ownership to take
+    constexpr void reset(mem::MemBlock ptr = mem::nullblk) noexcept(
+        std::is_nothrow_destructible_v<T>)
+    {
+      auto cpy = blk;
+      blk      = ptr;
+      if (cpy.is_null())
+        return;
+      ON_SCOPE_EXIT
+      {
+        ALLOCATOR::dealloc(cpy);
+      };
+      //run destructor
+      static_cast<T*>(cpy.ptr())->~T();
+    }
+
+    /// @brief Returns a reference to the owned MemBlock
+    /// @return Reference to the owned MemBlock
+    constexpr const mem::MemBlock& get() const noexcept { return blk; }
+
+    /// @brief Check if the owned MemBlock is null
+    /// @return True if null
+    constexpr bool is_null() const noexcept { return get().is_null(); }
+
+    /// @brief Check if the owned MemBlock is not null
+    explicit constexpr operator bool() const noexcept { return !is_null(); }
+
+    /// @brief Dereference operator
+    /// @return Dereferences pointer
+    constexpr std::add_lvalue_reference_t<T> operator*() const
+        noexcept(noexcept(*std::declval<T*>()))
+    {
+      assert_true("unique_ptr was null!", !is_null());
+      return *static_cast<T*>(blk.ptr());
+    }
+
+    /// @brief Dereference operator
+    /// @return Dereferences pointer
+    constexpr T* operator->() const noexcept
+    {
+      assert_true("unique_ptr was null!", !is_null());
+      return *static_cast<T*>(blk.ptr());
+    }
+  };
+
+  template<typename T, typename... Args>
+  /// @brief Constructs a UniquePtr using a global allocator
+  /// @tparam T The type to construct
+  /// @param args... The arguments to forward to the constructor
+  /// @return UniquePtr
+  constexpr auto make_unique(Args&&... args) noexcept(
+      std::is_nothrow_constructible_v<T, Args...>)
+  {
+    using namespace mem;
+
+    auto blk = GlobalAllocator.alloc(sizeof(T));
+    if constexpr (std::is_nothrow_constructible_v<T, Args...>)
+    {
+      new (blk.ptr()) T(std::forward<Args>(args)...);
+    }
+    else
+    {
+      try
+      {
+        new (blk.ptr()) T(std::forward<Args>(args)...);
+      }
+      catch (...)
+      {
+        GlobalAllocator.dealloc(blk);
+        throw;
+      }
+    }
+    return UniquePtr<T, decltype(GlobalAllocator)>(GlobalAllocator, blk);
+  }
+
+  template<typename T, meta::Allocator Alloc, typename... Args>
+  /// @brief Constructs a UniquePtr using a local allocator
+  /// @tparam T The type to construct
+  /// @param ref The local allocator
+  /// @param args... The arguments to forward to the constructor
+  /// @return UniquePtr
+  constexpr auto make_local_unique(Alloc& ref, Args&&... args) noexcept(
+      std::is_nothrow_constructible_v<T, Args...>)
+  {
+    auto blk = ref.alloc(sizeof(T));
+    if constexpr (std::is_nothrow_constructible_v<T, Args...>)
+    {
+      new (blk.ptr()) T(std::forward<Args>(args)...);
+    }
+    else
+    {
+      try
+      {
+        new (blk.ptr()) T(std::forward<Args>(args)...);
+      }
+      catch (...)
+      {
+        ref.dealloc(blk);
+        throw;
+      }
+    }
+    return UniquePtr<T, mem::LocalAllocatorRef<std::remove_cvref_t<decltype(ref)>>>(ref, blk);
+  }
+} // namespace clt
+
+#endif // !HG_COLT_SMART_POINTERS
--- include/colt/mem/allocator_ref.h
+++ include/colt/mem/allocator_ref.h
@@ -6,35 +6,32 @@
 
 namespace clt::mem
 {
-  template<fn_alloc_t ALLOC, fn_dealloc_t DEALLOC, fn_owns_t OWNS,
-    fn_expand_t EXPAND, fn_realloc_t REALLOC>
-    requires (ALLOC != nullptr) && (DEALLOC != nullptr)
+  template<
+      u64 ALIGN, fn_alloc_t ALLOC, fn_dealloc_t DEALLOC, fn_owns_t OWNS, fn_expand_t EXPAND,
+      fn_realloc_t REALLOC>
+    requires(ALLOC != nullptr) && (DEALLOC != nullptr)
   struct GlobalAllocatorRef
   {
-    MemBlock alloc(u64 sz)
-    {
-      return ALLOC(sz);
-    }
+    static constexpr u64 alignment = ALIGN;
 
-    void dealloc(MemBlock sz)
-    {
-      DEALLOC(sz);
-    }
+    MemBlock alloc(u64 sz) const { return ALLOC(sz); }
 
-    bool owns(MemBlock sz)
-      requires (OWNS != nullptr)
+    void dealloc(MemBlock sz) const { DEALLOC(sz); }
+
+    bool owns(MemBlock sz) const
+      requires(OWNS != nullptr)
     {
       return OWNS(sz);
     }
 
-    mem::MemBlock expand(mem::MemBlock sz, u64 dt)
-      requires (EXPAND != nullptr)
+    mem::MemBlock expand(mem::MemBlock sz, u64 dt) const
+      requires(EXPAND != nullptr)
     {
       return EXPAND(sz, dt);
     }
 
-    mem::MemBlock realloc(mem::MemBlock sz, u64 dt)
-      requires (REALLOC != nullptr)
+    mem::MemBlock realloc(mem::MemBlock sz, u64 dt) const
+      requires(REALLOC != nullptr)
     {
       return REALLOC(sz, dt);
     }
@@ -48,46 +45,41 @@ namespace clt::mem
   public:
     LocalAllocatorRef() = delete;
     LocalAllocatorRef(T& ptr) noexcept
-      : ptr(&ptr)
+        : ptr(&ptr)
     {
     }
-    
-    LocalAllocatorRef(LocalAllocatorRef&&) = default;
-    LocalAllocatorRef& operator=(LocalAllocatorRef&&) = default;
-    LocalAllocatorRef(const LocalAllocatorRef&) = default;
+
+    static constexpr u64 alignment = T::alignment;
+
+    LocalAllocatorRef(LocalAllocatorRef&&)                 = default;
+    LocalAllocatorRef& operator=(LocalAllocatorRef&&)      = default;
+    LocalAllocatorRef(const LocalAllocatorRef&)            = default;
     LocalAllocatorRef& operator=(const LocalAllocatorRef&) = default;
 
-    MemBlock alloc(u64 sz)
-    {
-      return ptr->alloc(sz);
-    }
+    MemBlock alloc(u64 sz) const { return ptr->alloc(sz); }
 
-    void dealloc(MemBlock sz)
-    {
-      ptr->dealloc(sz);
-    }
+    void dealloc(MemBlock sz) const { ptr->dealloc(sz); }
 
-    bool owns(MemBlock sz)
+    bool owns(MemBlock sz) const
       requires meta::OwningAllocator<T>
     {
       return ptr->owns(sz);
     }
 
-    mem::MemBlock expand(mem::MemBlock sz, u64 dt)
+    mem::MemBlock expand(mem::MemBlock sz, u64 dt) const
       requires meta::ExpandingAllocator<T>
     {
       return ptr->expand(sz, dt);
     }
 
-    mem::MemBlock realloc(mem::MemBlock sz, u64 dt)
+    mem::MemBlock realloc(mem::MemBlock sz, u64 dt) const
       requires meta::ReallocatableAllocator<T>
     {
       return ptr->realloc(sz, dt);
     }
   };
 
-  inline FreeList<Mallocator, 16, size_t{4096}, size_t{4096}>
-      DefaultGlobalAllocator;
+  inline FreeList<Mallocator, 16, size_t{4096}, size_t{4096}> DefaultGlobalAllocator;
 
   inline MemBlock global_alloc(u64 size) noexcept
   {
@@ -100,8 +92,8 @@ namespace clt::mem
   }
 
   static constexpr GlobalAllocatorRef<
-      &global_alloc, &global_dealloc, nullptr, nullptr, nullptr>
-      GlobalAllocator;
-}
-
+      decltype(DefaultGlobalAllocator)::alignment, &global_alloc, &global_dealloc, nullptr, nullptr,
+      nullptr>
+      GlobalAllocator;  
+} // namespace clt::mem
 #endif //!HG_ALLOCATOR_REF
