aa950451f6577a52e57308edcaa6844eb302c577
R533-Code <raphaeldibnehmepc@gmail.com>
2024-12-19 12:48:28 +0100

    Moved ViewOfFile to mmap

--- include/colt/io/mmap.cpp
+++ include/colt/io/mmap.cpp
@@ -1,7 +1,17 @@
 #include "mmap.h"
 
 #ifdef COLT_WINDOWS
-#include <Windows.h>
+  #define NOMINMAX
+  #define WIN32_LEAN_AND_MEAN
+  #include <Windows.h>
+#elif defined(COLT_LINUX) || defined(COLT_APPLE)
+  #include <sys/mman.h>
+  #include <sys/stat.h>
+  #include <fcntl.h>
+  #include <unistd.h>
+#endif // COLT_WINDOWS
+
+#ifdef COLT_WINDOWS
 
 namespace clt
 {
@@ -52,9 +62,12 @@ namespace clt
     return bytes{PAGE_SIZE};
   }
 
-  void VirtualPage::flush_icache() noexcept
+  void VirtualPage::flush_icache(const void* start, size_t offset) noexcept
   {
-    FlushInstructionCache(GetCurrentProcess(), this->ptr(), this->size());
+    assert_true(
+        "If start is not null, offset must not be 0!",
+        implies(start != nullptr, offset != 0));
+    FlushInstructionCache(GetCurrentProcess(), start, offset);
     // use __builtin___clear_cache() for linux
   }
 
@@ -63,3 +76,109 @@ namespace clt
 #else // !COLT_WINDOWS
 
 #endif // COLT_WINDOWS
+
+#include "mmap_file.h"
+
+namespace clt
+{
+#ifdef COLT_WINDOWS
+  bool ViewOfFile::is_closed() const noexcept
+  {
+    return file_handle == nullptr && mapping_handle == nullptr
+           && view_map == nullptr;
+  }
+
+  void ViewOfFile::close()
+  {
+    if (view_map != nullptr)
+    {
+      UnmapViewOfFile(view_map);
+      view_map = nullptr;
+    }
+    if (mapping_handle != nullptr)
+    {
+      CloseHandle((HANDLE)mapping_handle);
+      mapping_handle = nullptr;
+    }
+    CloseHandle((HANDLE)file_handle);
+    file_handle = nullptr;
+  }
+
+  Option<View<u8>> ViewOfFile::view() const noexcept
+  {
+    if (file_handle == nullptr)
+      return View<u8>{(const u8*)nullptr, (size_t)0};
+    LARGE_INTEGER value;
+    if (GetFileSizeEx(file_handle, &value) == 0)
+      return None;
+    return View<u8>{(const u8*)view_map, (size_t)value.QuadPart};
+  }
+
+  Option<ViewOfFile> ViewOfFile::open(const char* ptr)
+  {
+    auto handle = CreateFile(
+        ptr, GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL, nullptr);
+    if (handle == INVALID_HANDLE_VALUE)
+      return None;
+    LARGE_INTEGER file_size;
+    if (GetFileSizeEx(handle, &file_size) && file_size.QuadPart == 0)
+      return ViewOfFile((void*)handle, nullptr, nullptr);
+
+    auto map = CreateFileMapping(handle, nullptr, PAGE_READONLY, 0, 0, nullptr);
+    if (map == nullptr)
+    {
+      CloseHandle(handle);
+      return None;
+    }
+
+    auto view = MapViewOfFile(map, FILE_MAP_READ, 0, 0, 0);
+    if (view == nullptr)
+    {
+      CloseHandle(map);
+      CloseHandle(handle);
+      return None;
+    }
+    return ViewOfFile((void*)handle, (void*)map, view);
+  }
+#else
+  bool ViewOfFile::is_closed() const noexcept
+  {
+    return mmap_handle == nullptr;
+  }
+
+  void ViewOfFile::close()
+  {
+    munmap(mmap_handle, (off_t)file_size);
+    mmap_handle = nullptr;
+  }
+
+  Option<View<u8>> ViewOfFile::view() const noexcept
+  {
+    return View<u8>{(const u8*)mmap_handle, file_size * (size_t)(mmap_handle != 0)};
+  }
+
+  Option<ViewOfFile> ViewOfFile::open(const char* ptr)
+  {
+    int fd = ::open(ptr, O_RDWR);
+    if (fd == -1)
+      return None;
+
+    struct stat sb;
+    if (fstat(fd, &sb) != 0)
+    {
+      ::close(fd);
+      return None;
+    }
+    off_t file_size = sb.st_size;
+    void* addr      = mmap(nullptr, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (addr == MAP_FAILED)
+    {
+      ::close(fd);
+      return None;
+    }
+    ::close(fd);
+    return ViewOfFile{addr, (size_t)file_size};
+  }
+#endif // COLT_WINDOWS
+} // namespace clt
--- include/colt/io/mmap.h
+++ include/colt/io/mmap.h
@@ -1,8 +1,9 @@
 #ifndef HG_COLT_MMAP
 #define HG_COLT_MMAP
 
-#include <span>
-#include <colt/alpha/units.h>
+#include <colt/typedefs.h>
+#include <colt/dsa/option.h>
+#include <colt/dsa/string_view.h>
 
 namespace clt
 {
@@ -78,8 +79,115 @@ namespace clt
     COLTCPP_EXPORT
     static bytes page_size() noexcept;
 
-    /// @brief Flushes the instruction cache
-    COLTCPP_EXPORT void flush_icache() noexcept;
+    /// @brief Flush the instruction cache.
+    /// This function should be called if an application generate or
+    /// modify code in memory.
+    /// @param start The start of the instruction memory to flush (can be null)
+    /// @param offset The size of the memory to flush (0 if start is null)
+    /// @pre implies(start != nullptr, offset != 0)
+    COLTCPP_EXPORT static void flush_icache(
+        const void* start = nullptr, size_t offset = 0) noexcept;
+
+    /// @brief Flushes the instruction cache of the current page
+    void flush_icache() noexcept { VirtualPage::flush_icache(ptr(), size()); }
+  };
+
+  /// @brief Represents a view over a read-only memory mapped file.
+  class ViewOfFile
+  {
+#ifdef COLT_WINDOWS
+    void* file_handle;
+    void* mapping_handle;
+    void* view_map;
+
+    ViewOfFile(void* file, void* mapping, void* view) noexcept
+        : file_handle(file)
+        , mapping_handle(mapping)
+        , view_map(view)
+    {
+    }
+
+  public:
+    ViewOfFile(ViewOfFile&& other) noexcept
+        : file_handle(std::exchange(other.file_handle, nullptr))
+        , mapping_handle(std::exchange(other.mapping_handle, nullptr))
+        , view_map(std::exchange(other.view_map, nullptr))
+    {
+    }
+
+    ViewOfFile& operator=(ViewOfFile&& other) noexcept
+    {
+      assert_true("Self assignment is prohibited!", &other != this);
+
+      std::swap(file_handle, other.file_handle);
+      std::swap(mapping_handle, other.mapping_handle);
+      std::swap(view_map, other.view_map);
+
+      return *this;
+    }
+#else
+    void* mmap_handle;
+    size_t file_size;
+
+    ViewOfFile(void* file, size_t size) noexcept
+        : mmap_handle(file)
+        , file_size(size)
+    {
+    }
+
+  public:
+    ViewOfFile(ViewOfFile&& other) noexcept
+        : mmap_handle(std::exchange(other.mmap_handle, nullptr))
+        , file_size(std::exchange(other.file_size, 0))
+    {
+    }
+
+    ViewOfFile& operator=(ViewOfFile&& other) noexcept
+    {
+      assert_true("Self assignment is prohibited!", &other != this);
+      std::swap(mmap_handle, other.mmap_handle);
+      std::swap(file_size, other.file_size);
+      return *this;
+    }
+#endif // COLT_WINDOWS
+
+    /// @brief Default constructor. Returns a library that is not open
+    ViewOfFile()                             = default;
+    ViewOfFile(const ViewOfFile&)            = delete;
+    ViewOfFile& operator=(const ViewOfFile&) = delete;
+
+    /// @brief Destructor, automatically closes associated resources
+    ~ViewOfFile()
+    {
+      if (is_open())
+        close();
+    }
+    /// @brief True if the current library is open/
+    /// @return True if not closed
+    bool is_open() const noexcept { return !is_closed(); }
+
+    /// @brief True if the current library is not open.
+    /// A default constructed library is closed.
+    /// @return True if not open
+    COLTCPP_EXPORT bool is_closed() const noexcept;
+    /// @brief Closes the view.
+    /// This is done automatically by the destructor.
+    COLTCPP_EXPORT void close();
+
+    /// @brief Returns a view of bytes over the file.
+    /// If the file is not opened, returns an empty view.
+    /// None is only returned on OS failures.
+    /// @return None on OS failures, else view over the file.
+    COLTCPP_EXPORT Option<View<u8>> view() const noexcept;
+
+    /// @brief Opens a view of a file
+    /// @param ptr The file path
+    /// @return None on errors or opened ViewOfFile
+    COLTCPP_EXPORT static Option<ViewOfFile> open(const char* ptr);
+    /// @brief Opens a view of a file
+    /// @param ptr The file path
+    /// @return None on errors or opened ViewOfFile
+    static Option<ViewOfFile> open(ZStringView ptr) { return open(ptr.c_str()); }
   };
 } // namespace clt
 
