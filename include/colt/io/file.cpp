41cf9d73b2e726154231bdf79f680f7da57b95e5
R533-Code <raphaeldibnehmepc@gmail.com>
2024-12-27 22:07:38 +0100

    Added linux implementation

--- include/colt/io/file.cpp
+++ include/colt/io/file.cpp
@@ -40,10 +40,11 @@ namespace clt
 
   File& File::get_null_device() noexcept
   {
-    static auto FILE = File::open("nul", FileAccess::Write).value_or(File(-1, FileAccess::Write));
+    static auto FILE =
+        File::open("nul", FileAccess::Write).value_or(File(-1, FileAccess::Write));
     return FILE;
   }
-  
+
   bool File::is_terminal() const noexcept
   {
     return is_open() && _isatty(this->fileno());
@@ -160,16 +161,15 @@ namespace clt
 
   static auto convert_access(File::FileAccess access, bool text_mode) noexcept
   {
-
     auto ret = text_mode ? _O_TEXT : _O_BINARY;
-    switch_no_default (access)
+    switch_no_default(access)
     {
     case clt::File::FileAccess::Read:
       return ret | _O_RDONLY;
     case clt::File::FileAccess::Write:
-      return ret | _O_WRONLY | O_CREAT;
+      return ret | _O_WRONLY | _O_CREAT;
     case clt::File::FileAccess::Append:
-      return ret | _O_WRONLY | O_APPEND | O_CREAT;
+      return ret | _O_WRONLY | _O_APPEND | _O_CREAT;
     case clt::File::FileAccess::Create:
       return ret | _O_WRONLY | _O_EXCL | _O_CREAT;
     }
@@ -184,6 +184,194 @@ namespace clt
     return Option<File>(File(handle, access));
   }
 
+} // namespace clt
+
+#else // linux
+
+  #include <sys/stat.h>
+  #include <unistd.h>
+  #include <fcntl.h>
+
+namespace clt
+{
+  void File::close() noexcept
+  {
+    close(handle);
+    handle = -1;
+  }
+
+  File& File::get_stdin() noexcept
+  {
+    static auto FILE = File{0, FileAccess::Read};
+    return FILE;
+  }
+  File& File::get_stdout() noexcept
+  {
+    static auto FILE = File{1, FileAccess::Write};
+    return FILE;
+  }
+  File& File::get_stderr() noexcept
+  {
+    static auto FILE = File{2, FileAccess::Write};
+    return FILE;
+  }
+
+  File& File::get_null_device() noexcept
+  {
+    static auto FILE = File::open("/dev/null", FileAccess::Write)
+                           .value_or(File(-1, FileAccess::Write));
+    return FILE;
+  }
+
+  bool File::is_terminal() const noexcept
+  {
+    return is_open() && isatty(this->fileno());
+  }
+
+  Option<std::chrono::file_clock::time_point> File::creation_time() const noexcept
+  {
+    using namespace std::chrono;
+
+    struct statx stx;
+    if (statx(this->fileno(), "", AT_EMPTY_PATH, STATX_BTIME, &stx) != 0)
+      return None;
+
+    if (!(stx.stx_mask & STATX_BTIME))
+      return None;
+
+    auto system_time = system_clock::time_point{
+        seconds(stx.stx_btime.tv_sec) + nanoseconds(stx.stx_btime.tv_nsec)};
+    return clock_cast<file_clock>(system_time);
+  }
+
+  Option<std::chrono::file_clock::time_point> File::access_time() const noexcept
+  {
+    using namespace std::chrono;
+
+    struct statx stx;
+    if (statx(this->fileno(), "", AT_EMPTY_PATH, STATX_ATIME, &stx) != 0)
+      return None;
+
+    if (!(stx.stx_mask & STATX_ATIME))
+      return None;
+
+    auto system_time = system_clock::time_point{
+        seconds(stx.stx_atime.tv_sec) + nanoseconds(stx.stx_atime.tv_nsec)};
+    return clock_cast<file_clock>(system_time);
+  }
+
+  Option<std::chrono::file_clock::time_point> File::write_time() const noexcept
+  {
+    using namespace std::chrono;
+
+    struct statx stx;
+    if (statx(this->fileno(), "", AT_EMPTY_PATH, STATX_MTIME, &stx) != 0)
+      return None;
+
+    if (!(stx.stx_mask & STATX_MTIME))
+      return None;
+
+    auto system_time = system_clock::time_point{
+        seconds(stx.stx_mtime.tv_sec) + nanoseconds(stx.stx_mtime.tv_nsec)};
+    return clock_cast<file_clock>(system_time);
+  }
+
+  Option<bytes> File::file_size() const noexcept
+  {
+    if (!is_open())
+      return None;
+    struct statx stx;
+    if (statx(this->fileno(), "", AT_EMPTY_PATH, STATX_SIZE, &stx) != 0)
+      return None;
+    if (!(stx.stx_mask & STATX_SIZE))
+      return None;
+    return bytes{stx.stx_size};
+  }
+
+  ErrorFlag File::flush() noexcept
+  {
+    if (!is_open() || fsync(this->fileno()) == -1)
+      return ErrorFlag::error();
+    return ErrorFlag::success();
+  }
+
+  Option<u8> File::read() noexcept
+  {
+    if (!is_open() || access != FileAccess::Read)
+      return None;
+    if (u8 ret; read(this->fileno(), &ret, 1) == 1)
+      return Option<u8>(ret);
+    return None;
+  }
+
+  Option<size_t> File::write(u8 out) noexcept
+  {
+    if (!is_open() || access == FileAccess::Read)
+      return None;
+    if (write(this->fileno(), &out, 1) == 1)
+      return 1;
+    return None;
+  }
+
+  Option<size_t> File::read(Span<u8> out) noexcept
+  {
+    if (!is_open() || access != FileAccess::Read)
+      return None;
+    // TODO: overflow check
+    auto read = read(this->fileno(), out.data(), out.size_bytes());
+    if (read < 0)
+      return None;
+    return Option<size_t>((size_t)read);
+  }
+
+  Option<size_t> File::write(View<u8> out) noexcept
+  {
+    if (!is_open() || access == FileAccess::Read)
+      return None;
+    // TODO: overflow check
+    auto write = _write(this->fileno(), &out, out.size_bytes());
+    if (write < 0)
+      return None;
+    return Option<size_t>((size_t)write);
+  }
+
+  bool File::is_eof() const noexcept
+  {
+    if (!is_open())
+      return false;
+    off_t current_pos = lseek(this->fileno(), 0, SEEK_CUR);
+    if (current_pos == -1)
+      return false;
+
+    struct stat file_stat;
+    if (fstat(this->fileno(), &file_stat) == -1)
+      return false;
+    return current_pos >= file_stat.st_size;
+  }
+
+  static auto convert_access(File::FileAccess access, bool text_mode) noexcept
+  {
+    switch_no_default(access)
+    {
+    case clt::File::FileAccess::Read:
+      return O_RDONLY;
+    case clt::File::FileAccess::Write:
+      return O_WRONLY | O_CREAT;
+    case clt::File::FileAccess::Append:
+      return O_WRONLY | O_APPEND | O_CREAT;
+    case clt::File::FileAccess::Create:
+      return O_WRONLY | O_EXCL | O_CREAT;
+    }
+  }
+
+  Option<File> File::open(
+      const char* const path, FileAccess access, bool text_mode) noexcept
+  {
+    auto handle = _open(path, convert_access(access, text_mode));
+    if (handle == -1)
+      return None;
+    return Option<File>(File(handle, access));
+  }
 } // namespace clt
 
 #endif // COLT_WINDOWS
