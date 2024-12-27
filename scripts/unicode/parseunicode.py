fba0c4511f25ca466892dbdbc8f91d3b22373ac1
R533-Code <raphaeldibnehmepc@gmail.com>
2024-12-27 11:03:47 +0100

    Improved unicode dara parser and added new files support

--- scripts/unicode/parseunicode.py
+++ scripts/unicode/parseunicode.py
@@ -17,20 +17,6 @@ MAX_PLANE      = 17
 os.chdir(os.path.dirname(PATH_THIS_FILE))
 
 @dataclasses.dataclass
-class CodePointInfo:
-  CODE_POINT : CodePoint
-  NAME       : str = ""
-  CATEGORY   : str = "Cn"
-  
-  DECIMAL :  int|None = None
-  DIGIT   :  int|None = None
-  NUMERIC : frac|None = None
-  
-  SIMPLE_UPPERCASE_MAPPING : CodePoint|None = None
-  SIMPLE_LOWERCASE_MAPPING : CodePoint|None = None
-  SIMPLE_TITLECASE_MAPPING : CodePoint|None = None
-
-@dataclasses.dataclass
 class Property:
   NAME     : str
   FULLNAME : str
@@ -74,7 +60,6 @@ def parse_propertyvalue()->dict[str, Property]:
     else:
       current = Property(NAME=NAME, KEYS=current_dict)
       PROPERTY_LIST[NAME] = current
-  
   return PROPERTY_LIST
 
 @functools.cache
@@ -89,19 +74,37 @@ def parse_propertyaliases()->dict[str, str]:
     split = [i.strip() for i in line.split(';')]
     NAME = split[0]
     VALUE = split[1]    
-    PROPERTY_LIST[NAME] = NAME
+    PROPERTY_LIST[NAME]  = NAME
     PROPERTY_LIST[VALUE] = NAME
     for i in range(2, len(split)):
       PROPERTY_LIST[split[i]] = NAME
   return PROPERTY_LIST
 
+@functools.cache
+def parse_properties()->list[tuple[CodePointRange, str, str]]:
+  """Parses all the properties from `PropList.txt`
+
+  Returns:
+      list[tuple[CodePointRange, str, str]]: CodePointRange, Property, Value
+  """
+  
+  PROPERTY_LIST : list[tuple[CodePointRange, str, str]] = []
+  REGEX = re.compile("(.*);(.*)# (..).*")  
+  for line in colt.lines_of(PATH_UCD + 'PropList.txt'):
+    match = REGEX.match(line)
+    assert match is not None
+    range = CodePointRange.from_str(match.group(1).strip())
+    prope = match.group(2).strip()
+    value = match.group(3).strip()
+    PROPERTY_LIST.append((range, prope, value))
+  return PROPERTY_LIST
+
 UNICODE_DATA_PROPERTIES = [
-  "_CODEPOINT",
   "Name",
   "General_Category",
   "Canonical_Combining_Class",
   "Bidi_Class",
-  "Decomposition_Type|Decomposition_Mapping",
+  "Decomposition_Type", # Decomposition_Mapping
   "Numeric_Type",
   "Numeric_Value",
   "Numeric_Value",
@@ -113,6 +116,14 @@ UNICODE_DATA_PROPERTIES = [
   "Simple_Titlecase_Mapping",
 ]
 
+@dataclasses.dataclass
+class CodePointInfo:
+  CODE_POINT : CodePoint
+  PROPERTIES : dict[str, str]
+  
+  def get(self, property):
+    return self.PROPERTIES[parse_propertyaliases()[property]]
+
 @functools.cache
 def parse_unicodedata()->list[CodePointInfo]:
   """Parses all the code point informations from UnicodeData.txt.
@@ -123,54 +134,83 @@ def parse_unicodedata()->list[CodePointInfo]:
       list[CodePointInfo]: The list of code points
   """
   
+  def default_to_true_default(default: str|None):    
+    if default is None:
+      return default
+    CONV = {
+      "<code point>" : CodePoint.int_to_hex(CODE_POINT.value),
+      "<none>"       : None,
+      "NaN"          : None,
+      "Unassigned"   : "Cn",
+    }
+    return CONV.get(default, default)
+  @functools.cache
+  def default_properties():
+    """
+    ONE ASSUMPTION MADE BY THIS FUNCTION IS THAT
+    THE CODE POINT VALUE DOES NOT CHANGE THE DEFAULT.
+    For Unicode 16.0.0 this appears to be true
+    """
+    ret = dict()
+    for j in range(0, len(UNICODE_DATA_PROPERTIES)):
+      current_abrv = PROPERTY_ALIAS[UNICODE_DATA_PROPERTIES[j]]
+      value = None
+      default = PROPERTY_LIST[current_abrv].DEFAULT_VALUE
+      if default is not None: # and CodePoint(i) in default[0]:
+        value = default_to_true_default(default[1])
+      # Lookup abbreviation in property alias
+      ret[current_abrv] = value
+    return ret
+  
   # starts with CodePointInfo(-1) to avoid a bound check inside the
   # parsing loop. This character will be removed when returning
   # the list
-  CODE_POINT_LIST : list[CodePointInfo] = [CodePointInfo(CodePoint(-1))]
+  CODE_POINT_LIST : list[CodePointInfo] = [CodePointInfo(CodePoint(-1), dict())]
   PROPERTY_LIST   = parse_propertyvalue()
   PROPERTY_ALIAS  = parse_propertyaliases()
-    
+  
   # See https://www.unicode.org/reports/tr44/#UnicodeData.txt
   for line in colt.lines_of(PATH_UCD + 'UnicodeData.txt'):
     split = [i.strip() for i in line.split(';')]
     
     CODE_POINT = CodePoint.from_str(split[0])
-    NAME       : str = str(split[1])    # The name (can be empty!)
-    CATEGORY   : str = str(split[2])    # See https://www.unicode.org/reports/tr44/#General_Category_Values        
+    split.pop(0)
+    PROPERTIES = dict()
+    for i in range(0, len(UNICODE_DATA_PROPERTIES)):
+      current_abrv = PROPERTY_ALIAS[UNICODE_DATA_PROPERTIES[i]]
+      value = None
+      default = PROPERTY_LIST[current_abrv].DEFAULT_VALUE
+      if len(split[i]) != 0:
+        value = split[i]
+      elif (default is not None and CODE_POINT in default[0]):
+        value = default_to_true_default(default[1])
+      # Lookup abbreviation in property alias
+      PROPERTIES[current_abrv] = value
     
-    DECIMAL :  int|None = colt.none_or(int, split[6])  # [0-9] or empty
-    DIGIT   :  int|None = colt.none_or(int, split[7])  # [0-9] or empty
-    NUMERIC : frac|None = colt.none_or(frac, split[8]) # [+-][0-9]+(/[0-9]) or empty
+    # TODO: Condition as per (14) Note
     
-    SIMPLE_UPPERCASE_MAPPING = colt.none_or(CodePoint.from_str, split[12]) # Uppercase code point number or empty
-    SIMPLE_LOWERCASE_MAPPING = colt.none_or(CodePoint.from_str, split[13]) # Lowercase code point number or empty
-    SIMPLE_TITLECASE_MAPPING = colt.none_or(CodePoint.from_str, split[14]) or SIMPLE_UPPERCASE_MAPPING # Condition as per (14) Note
-    
-    current = CodePointInfo(
-      CODE_POINT, NAME, CATEGORY, DECIMAL, DIGIT, NUMERIC,
-      SIMPLE_UPPERCASE_MAPPING, SIMPLE_LOWERCASE_MAPPING,
-      SIMPLE_TITLECASE_MAPPING)
+    current = CodePointInfo(CODE_POINT, PROPERTIES)
     # RANGES NEED SPECIAL HANDLING:
     # if a name follows: <.*, First>
     # then the next is : <.*, Last>
     # we need to duplicate the first
     previous_cp = CODE_POINT_LIST[-1].CODE_POINT.value
     if CODE_POINT.value != previous_cp + 1:
-      if NAME.endswith(", Last>"):
+      if current.get("Name").endswith(", Last>"):
         CODE_POINT_LIST += [dataclasses.replace(current, CODE_POINT=CodePoint(i + 1)) for i in range(previous_cp, CODE_POINT.value)]
       else:
-        CODE_POINT_LIST += [CodePointInfo(CodePoint(i)) for i in range(previous_cp + 1, CODE_POINT.value)]
+        CODE_POINT_LIST += [CodePointInfo(CodePoint(i), default_properties()) for i in range(previous_cp + 1, CODE_POINT.value)]
         CODE_POINT_LIST.append(current)
     else:
       CODE_POINT_LIST.append(current)
   # unassigned noncharacters
   while len(CODE_POINT_LIST) <= CodePoint.max_code_point() + 1:
-    CODE_POINT_LIST.append(CodePointInfo(len(CODE_POINT_LIST) - 1))    
+    CODE_POINT_LIST.append(CodePointInfo(len(CODE_POINT_LIST) - 1, CODE_POINT_LIST[-1].PROPERTIES))    
   # pop the CodePointInfo(-1)
   return CODE_POINT_LIST[1:]
 
 def is_assigned_character(x: CodePointInfo)->bool:
-  return (x.CATEGORY != "Cn"
-    and x.CATEGORY != "Co"
-    and x.CATEGORY != "Cs"
+  return (x.get("General_Category") != "Cn"
+    and x.get("General_Category") != "Co"
+    and x.get("General_Category") != "Cs"
   )
