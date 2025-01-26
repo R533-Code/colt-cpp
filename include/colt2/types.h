3d2f7aec00f258372632469d8946bd65b1f5b743
R533-Code <raphaeldibnehmepc@gmail.com>
2025-01-26 10:52:33 +0100

    Added assertion for notnull_ptr

--- include/colt2/types.h
+++ include/colt2/types.h
@@ -38,6 +38,120 @@
   type& operator=(type&&)      = delete;    \
   type& operator=(const type&) = delete;
 
+#pragma region Assertions Macros
+/*********************************************/
+// ASSERTIONS MACROS
+/*********************************************/
+
+namespace clt
+{
+  [[noreturn]]
+  /// @brief Marks a branch as unreachable, printing an error on Debug build
+  /// @param error The error message
+  /// @param src The source code information
+  void unreachable(
+      const char* error, clt::source_location src = clt::source_location::current());
+} // namespace clt
+
+namespace clt::details
+{
+  /// @brief Responsible of storing an expressions value and its source code string
+  struct Assertion
+  {
+    /// @brief The string representing the value
+    const char* str;
+    /// @brief The value of the assertion
+    bool value;
+  };
+
+  /// @brief Used to cause a compile-time failure
+  inline void constexpr_assert_true_failed() noexcept
+  {
+    //ASSERTION FAILED AT COMPILE TIME!
+  }
+
+  template<typename... BoolTs>
+    requires(sizeof...(BoolTs) != 0)
+  /// @brief Asserts that multiple conditions are true, and if not,
+  /// stops the application and prints the failed assertions.
+  /// @tparam ...BoolTs The type parameter pack
+  /// @param message The message to print
+  /// @param src The source location
+  /// @param ...bools The Assertion pack
+  constexpr void assert_true_multiple(
+      const char* message, clt::source_location src, BoolTs... bools) noexcept
+  {
+    static_assert(
+        (std::is_same_v<Assertion, std::remove_cvref_t<BoolTs>> && ...),
+        "This function expects 'Assertion'! Use assert_true rather than calling it "
+        "directly!");
+    if (std::is_constant_evaluated())
+    {
+      Assertion* array[sizeof...(BoolTs)] = {&bools...};
+      for (size_t i = 0; i < sizeof...(BoolTs); i++)
+      {
+        if (array[i]->value)
+          continue;
+        constexpr_assert_true_failed();
+      }
+    }
+    else if constexpr (is_debug_build())
+    {
+      Assertion* array[sizeof...(BoolTs)] = {&bools...};
+
+      bool error = false;
+      for (size_t i = 0; i < sizeof...(BoolTs); i++)
+      {
+        if (array[i]->value)
+          continue;
+        if (!error)
+        {
+          std::printf(
+              "FATAL: Assertion failed in function '%s' (line %u) in "
+              "file:\n'%s'\n%s",
+              src.function_name(), src.line(), src.file_name(), message);
+          error = true;
+        }
+        std::printf("%u. %s evaluated to false", (u32)(i + 1), array[i]->str);
+      }
+      if (error)
+        clt::debug_break();
+    }
+  }
+} // namespace clt::details
+
+/// @brief Helper for transforming assertions into strings and their evaluated value
+#define __DETAILS__COLT_TO_ASSERTION(expr) \
+  , clt::details::Assertion                \
+  {                                        \
+    #expr, (expr)                          \
+  }
+
+#ifndef COLT_MSVC
+  #define __DETAILS__COLT_TO_ASSUME(expr)
+#else
+  #define __DETAILS__COLT_TO_ASSUME(expr) , HEDLEY_ASSUME(expr)
+#endif // COLT_CLANG
+
+/// @brief Asserts that all condition are true
+#define assert_true(MESSAGE, COND, ...)                                           \
+  clt::details::assert_true_multiple(                                             \
+      MESSAGE, clt::source_location::current() __DETAILS__COLT_TO_ASSERTION(COND) \
+                   COLT_FOR_EACH(__DETAILS__COLT_TO_ASSERTION, __VA_ARGS__))      \
+      __DETAILS__COLT_TO_ASSUME(COND)                                             \
+          COLT_FOR_EACH(__DETAILS__COLT_TO_ASSUME, __VA_ARGS__)
+
+/// @brief switch case with no default
+#define switch_no_default(...)                                    \
+  switch (__VA_ARGS__)                                            \
+  default:                                                        \
+    if (true)                                                     \
+    {                                                             \
+      clt::unreachable("Invalid value for 'switch_no_default'."); \
+    }                                                             \
+    else
+#pragma endregion
+
 namespace clt
 {
   COLT_FORCE_INLINE
@@ -397,6 +511,7 @@ namespace clt
     constexpr notnull_ptr(T* ptr) noexcept
         : ptr_(ptr)
     {
+      assert_true("notnull_ptr was assigned a null value!", ptr_ != nullptr);
     }
     MAKE_DEFAULT_COPY_AND_MOVE_FOR(notnull_ptr);
 
@@ -435,118 +550,6 @@ namespace clt
     }
   };
 } // namespace clt
-
-/*********************************************/
-// ASSERTIONS MACROS
-/*********************************************/
-
-namespace clt
-{
-  [[noreturn]]
-  /// @brief Marks a branch as unreachable, printing an error on Debug build
-  /// @param error The error message
-  /// @param src The source code information
-  void unreachable(
-      const char* error, clt::source_location src = clt::source_location::current());
-} // namespace clt
-
-namespace clt::details
-{
-  /// @brief Responsible of storing an expressions value and its source code string
-  struct Assertion
-  {
-    /// @brief The string representing the value
-    const char* str;
-    /// @brief The value of the assertion
-    bool value;
-  };
-
-  /// @brief Used to cause a compile-time failure
-  inline void constexpr_assert_true_failed() noexcept
-  {
-    //ASSERTION FAILED AT COMPILE TIME!
-  }
-
-  template<typename... BoolTs>
-    requires(sizeof...(BoolTs) != 0)
-  /// @brief Asserts that multiple conditions are true, and if not,
-  /// stops the application and prints the failed assertions.
-  /// @tparam ...BoolTs The type parameter pack
-  /// @param message The message to print
-  /// @param src The source location
-  /// @param ...bools The Assertion pack
-  constexpr void assert_true_multiple(
-      const char* message, clt::source_location src, BoolTs... bools) noexcept
-  {
-    static_assert(
-        (std::is_same_v<Assertion, std::remove_cvref_t<BoolTs>> && ...),
-        "This function expects 'Assertion'! Use assert_true rather than calling it "
-        "directly!");
-    if (std::is_constant_evaluated())
-    {
-      Assertion* array[sizeof...(BoolTs)] = {&bools...};
-      for (size_t i = 0; i < sizeof...(BoolTs); i++)
-      {
-        if (array[i]->value)
-          continue;
-        constexpr_assert_true_failed();
-      }
-    }
-    else if constexpr (is_debug_build())
-    {
-      Assertion* array[sizeof...(BoolTs)] = {&bools...};
-
-      bool error = false;
-      for (size_t i = 0; i < sizeof...(BoolTs); i++)
-      {
-        if (array[i]->value)
-          continue;
-        if (!error)
-        {
-          std::printf(
-              "FATAL: Assertion failed in function '%s' (line %u) in "
-              "file:\n'%s'\n%s",
-              src.function_name(), src.line(), src.file_name(), message);
-          error = true;
-        }
-        std::printf("%u. %s evaluated to false", (u32)(i + 1), array[i]->str);
-      }
-      if (error)
-        clt::debug_break();
-    }
-  }
-} // namespace clt::details
-
-/// @brief Helper for transforming assertions into strings and their evaluated value
-#define __DETAILS__COLT_TO_ASSERTION(expr) \
-  , clt::details::Assertion                \
-  {                                        \
-    #expr, (expr)                          \
-  }
-
-#ifndef COLT_MSVC
-  #define __DETAILS__COLT_TO_ASSUME(expr)
-#else
-  #define __DETAILS__COLT_TO_ASSUME(expr) , HEDLEY_ASSUME(expr)
-#endif // COLT_CLANG
-
-/// @brief Asserts that all condition are true
-#define assert_true(MESSAGE, COND, ...)                                           \
-  clt::details::assert_true_multiple(                                             \
-      MESSAGE, clt::source_location::current() __DETAILS__COLT_TO_ASSERTION(COND) \
-                   COLT_FOR_EACH(__DETAILS__COLT_TO_ASSERTION, __VA_ARGS__))      \
-      __DETAILS__COLT_TO_ASSUME(COND)                                             \
-          COLT_FOR_EACH(__DETAILS__COLT_TO_ASSUME, __VA_ARGS__)
-
-/// @brief switch case with no default
-#define switch_no_default(...)                                    \
-  switch (__VA_ARGS__)                                            \
-  default:                                                        \
-    if (true)                                                     \
-    {                                                             \
-      clt::unreachable("Invalid value for 'switch_no_default'."); \
-    }                                                             \
-    else
 
 namespace clt::details
 {
